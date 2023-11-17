f0116aff89eb18c6100c97c11848b402a4b703e6
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-11-17 21:12:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 echo "Packaging python environment"
+npx kill-port 8000
+rm -rf src-tauri/binaries/
+rm -rf pyserver/build/
 cd src-tauri
 mkdir binaries
 cd ..
 cd pyserver
 pyoxidizer build
 cd ..
-cp +r pyserver/build src-tauri/binaries/
+cp -r pyserver/build src-tauri/binaries/
 echo "Done packaging python environment"
-
