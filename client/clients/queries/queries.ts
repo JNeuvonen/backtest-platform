dfd3eb4fb9165d3d38e4ce9083ab8196366f5a3a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-10 12:34:09 +0200

    Refactor

--- client/clients/queries/queries.ts
+++ client/clients/queries/queries.ts
@@ -1,32 +1,11 @@
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
-import { QUERY_KEYS } from "../utils/query-keys";
-import { fetchAllTickers, fetchDatasets } from "./requests";
-
-export interface DatasetMetadata {
-  columns: string[];
-  start_date: number;
-  end_date: number;
-  table_name: string;
-}
-
-interface DatasetsResponse {
-  res: {
-    tables: DatasetMetadata[];
-  };
-  status: number;
-}
-
-export interface BinanceBasicTicker {
-  symbol: string;
-  price: number;
-}
-
-interface BinanceTickersResponse {
-  res: {
-    pairs: BinanceBasicTicker[];
-  };
-  status: number;
-}
+import { QUERY_KEYS } from "../../utils/query-keys";
+import { fetchAllTickers, fetchDataset, fetchDatasets } from "../requests";
+import {
+  BinanceTickersResponse,
+  DatasetResponse,
+  DatasetsResponse,
+} from "./response-types";
 
 export function useDatasetsQuery(): UseQueryResult<DatasetsResponse, unknown> {
   return useQuery<DatasetsResponse, unknown>({
@@ -42,5 +21,14 @@ export function useBinanceTickersQuery(): UseQueryResult<
   return useQuery<BinanceTickersResponse, unknown>({
     queryKey: [QUERY_KEYS.fetch_binance_tickers],
     queryFn: fetchAllTickers,
+  });
+}
+
+export function useDatasetQuery(
+  datasetName: string
+): UseQueryResult<DatasetResponse, unknown> {
+  return useQuery<DatasetResponse, unknown>({
+    queryKey: [QUERY_KEYS.fetch_datasets, datasetName],
+    queryFn: () => fetchDataset(datasetName),
   });
 }
---
+++ client/clients/queries/response-types.ts
@@ -0,0 +1,75 @@
+export interface DatasetMetadata {
+  columns: string[];
+  start_date: number;
+  end_date: number;
+  table_name: string;
+}
+
+export interface DatasetsResponse {
+  res: {
+    tables: DatasetMetadata[];
+  };
+  status: number;
+}
+
+export interface BinanceBasicTicker {
+  symbol: string;
+  price: number;
+}
+
+export interface BinanceTickersResponse {
+  res: {
+    pairs: BinanceBasicTicker[];
+  };
+  status: number;
+}
+
+export interface DatasetResponse {
+  res: {
+    dataset: Dataset;
+  };
+  status: number;
+}
+
+export interface Dataset {
+  columns: string[];
+  head: Array<number[]>;
+  tail: Array<number[]>;
+  null_counts: NullCounts;
+  row_count: number;
+  stats_by_col: StatsByCol;
+}
+
+export interface NullCounts {
+  kline_open_time: number;
+  open_price: number;
+  high_price: number;
+  low_price: number;
+  close_price: number;
+  volume: number;
+  quote_asset_volume: number;
+  number_of_trades: number;
+  taker_buy_base_asset_volume: number;
+  taker_buy_quote_asset_volume: number;
+}
+
+export interface StatsByCol {
+  kline_open_time: StatsCol;
+  open_price: StatsCol;
+  high_price: StatsCol;
+  low_price: StatsCol;
+  close_price: StatsCol;
+  volume: StatsCol;
+  quote_asset_volume: StatsCol;
+  number_of_trades: StatsCol;
+  taker_buy_base_asset_volume: StatsCol;
+  taker_buy_quote_asset_volume: StatsCol;
+}
+
+export interface StatsCol {
+  mean: number;
+  median: number;
+  min: number;
+  max: number;
+  std_dev: number;
+}
