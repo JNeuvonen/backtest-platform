dc45aab77d2bfb1dc2456686f2fd09c5fd5da512
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-10 12:34:42 +0200

    Update Dataset.tsx

--- client/pages/Available/Dataset.tsx
+++ client/pages/Available/Dataset.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useDatasetQuery } from "../../clients/queries/queries";
+import { Box, Spinner, Table, Th, Thead, Tr } from "@chakra-ui/react";
+import { GenericTable } from "../../components/tables/GenericTable";
 
 type DatasetDetailParams = {
   datasetName: string;
@@ -8,5 +11,32 @@ type DatasetDetailParams = {
 export const DatasetDetailPage = () => {
   const params = useParams<DatasetDetailParams>();
   const datasetName = params.datasetName || "";
-  return <div>{datasetName}</div>;
+  const { data, isLoading } = useDatasetQuery(datasetName);
+
+  const columnOnClickFunction = (selectedColumn: string) => {};
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const dataset = data?.res.dataset;
+
+  if (!dataset) {
+    return <Box>Page is not available</Box>;
+  }
+
+  const columns = dataset.columns;
+
+  return (
+    <div>
+      <Box>{datasetName}</Box>
+      <Box marginTop={"16px"}>
+        <GenericTable
+          columns={columns}
+          rows={dataset.head}
+          columnOnClickFunc={columnOnClickFunction}
+        />
+      </Box>
+    </div>
+  );
 };
