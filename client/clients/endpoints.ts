d66925a7ff84723072ab248a66ded5034250af89
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-10 15:25:19 +0200

    Update endpoints.ts

--- client/clients/endpoints.ts
+++ client/clients/endpoints.ts
@@ -18,6 +18,8 @@ export const URLS = {
   get_tables: BASE_URL + API.tables,
   get_table: (datasetName: string) =>
     BASE_URL + API.dataset + `/${datasetName}`,
+  get_column: (datasetName: string, columnName: string) =>
+    BASE_URL + API.dataset + `/${datasetName}/col-info/${columnName}`,
   binance_get_all_tickers: BASE_URL + API.binance.get_all_tickers,
   binance_fetch_klines: BASE_URL + API.binance.fetch_klines,
   ws_streams_log: API.streams.log,
