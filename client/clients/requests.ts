79ec745d5b5e7ff0df5f25e1f437236cdf9162a4
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-10 12:34:27 +0200

    Add fetchDataset

--- client/clients/requests.ts
+++ client/clients/requests.ts
@@ -6,6 +6,11 @@ export async function fetchDatasets() {
   return buildRequest({ method: "GET", url });
 }
 
+export async function fetchDataset(datasetName: string) {
+  const url = URLS.get_table(datasetName);
+  return buildRequest({ method: "GET", url });
+}
+
 export async function fetchAllTickers() {
   const url = URLS.binance_get_all_tickers;
   return buildRequest({ method: "GET", url });
