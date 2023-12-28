648ddeb724e6d74782007e99c5a01907a9a722ee
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-28 10:54:32 +0200

    Update server.py

--- pyserver/src/server.py
+++ pyserver/src/server.py
@@ -7,18 +7,13 @@ from db import (
     create_connection,
     get_column_names,
     get_tables,
-    insert_binance_scrape_job,
-    poll_scrape_jobs,
 )
 from pydantic import BaseModel
-from bina_load_data import ScrapeJob
 from typing import Optional, List
-from constants import DB_DATASETS, DB_WORKER_QUEUE
-from log import get_logger
+from constants import DB_DATASETS
 from routes_binance import router as binance_router
 from route_datasets import router as datasets_router
 from streams import router as streams_router
-import threading
 
 app = FastAPI()
 app.include_router(binance_router, prefix="/binance")
@@ -64,49 +59,6 @@ async def read_tables():
     except sqlite3.Error as e:
         error_detail = {"error": str(e), "db_path": db_path}
         raise HTTPException(status_code=500, detail=error_detail)
-
-
-@app.post("/create_scrape_job")
-def create_scrape_job(scrape_job: ScrapeJobModel):
-    job = ScrapeJob(
-        url=scrape_job.url,
-        target_dataset=False
-        if scrape_job.target_dataset is None
-        else scrape_job.target_dataset,
-        columns_to_drop=scrape_job.columns_to_drop,
-    )
-
-    if job.path is None:
-        return {"message": "invalid URL provided"}
-
-    db_worker_queue_path = get_path(DB_WORKER_QUEUE)
-    worker_queue_conn = create_connection(db_worker_queue_path)
-    insert_success = insert_binance_scrape_job(worker_queue_conn, job)
-
-    if insert_success:
-        return {"message": "ScrapeJob created successfully", "scrape_job": job}
-
-    return Response(
-        content="ScrapeJob was not created succesfully",
-        media_type="text/plain",
-        status_code=status.HTTP_400_BAD_REQUEST,
-    )
-
-
-@app.post("/poll_scrape_jobs")
-def poll_scrape_jobs_endpoint():
-    logger = get_logger()
-    logger.info("Started worker thread for polling scrape jobs.")
-    worker_thread = threading.Thread(
-        target=poll_scrape_jobs, args=(APP_DATA_PATH, logger)
-    )
-    worker_thread.start()
-
-    return Response(
-        content="Initiated worker queue poll",
-        media_type="text/plain",
-        status_code=status.HTTP_200_OK,
-    )
 
 
 @app.get("/tables/{table_name}/columns")
