e43dec3016d9daef656498f4bdde812f2c64a34f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-29 15:12:52 +0200

    Update sql_statements.py

--- pyserver/src/sql_statements.py
+++ pyserver/src/sql_statements.py
@@ -4,7 +4,7 @@ from constants import DATASET_UTIL_TABLE_NAME, DatasetUtilsColumns
 CREATE_DATASET_UTILS_TABLE = f"""
     CREATE TABLE IF NOT EXISTS {DATASET_UTIL_TABLE_NAME} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        {DatasetUtilsColumns.TABLE_NAME.value} TEXT NOT NULL,
-        {DatasetUtilsColumns.TIMESERIES_COLUMN.value} TEXT NOT NULL
+        {DatasetUtilsColumns.DATASET_NAME.value} TEXT NOT NULL,
+        {DatasetUtilsColumns.TIMESERIES_COLUMN.value} TEXT
     );
 """
