5e7f9a4a83e8b667cc94a24016bfdc90e7f0239c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-29 15:12:45 +0200

    Update confirm.tsx

--- client/components/form/confirm.tsx
+++ client/components/form/confirm.tsx
@@ -6,20 +6,20 @@ import { BUTTON_VARIANTS } from "../../theme";
 interface ConfirmProps {
   isOpen: boolean;
   onClose: () => void;
-  title: string;
-  message: string | React.ReactNode;
-  confirmText: string;
-  cancelText: string;
   onConfirm: () => void;
+  title?: string;
+  message?: string | React.ReactNode;
+  confirmText?: string;
+  cancelText?: string;
 }
 
 export const ConfirmModal: React.FC<ConfirmProps> = ({
   isOpen,
   onClose,
-  title,
-  message,
-  confirmText,
-  cancelText,
+  title = "Confirm",
+  message = "Are you sure you want to confirm this action?",
+  confirmText = "Confirm",
+  cancelText = "Cancel",
   onConfirm,
 }) => {
   return (
