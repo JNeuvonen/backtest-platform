f1bc77086ded32482593d2168ce7264b0f612395
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-29 15:12:51 +0200

    Update route_datasets.py

--- pyserver/src/route_datasets.py
+++ pyserver/src/route_datasets.py
@@ -1,13 +1,15 @@
 import os
 from fastapi import APIRouter, Body, HTTPException
-from constants import DB_DATASETS
+from constants import DB_DATASETS, DB_DATASETS_UTIL
+from pydantic import BaseModel
 
 from db import (
     create_connection,
     get_column_detailed_info,
     get_dataset_table,
-    get_table_utils_info,
+    get_timeseries_col,
     rename_column,
+    update_timeseries_col,
 )
 
 
@@ -24,10 +26,27 @@ async def route_get_dataset_by_name(dataset_name: str) -> dict:
 
 @router.get("/{dataset_name}/col-info/{column_name}")
 async def route_get_dataset_col_info(dataset_name: str, column_name: str) -> dict:
-    conn = create_connection(os.path.join(APP_DATA_PATH, DB_DATASETS))
-    col_info = get_column_detailed_info(conn, dataset_name, column_name)
-    table_utils = get_table_utils_info(conn, dataset_name)
-    return {"column": col_info}
+    datasets_conn = create_connection(os.path.join(APP_DATA_PATH, DB_DATASETS))
+    utils_conn = create_connection(os.path.join(APP_DATA_PATH, DB_DATASETS_UTIL))
+    timeseries_col = get_timeseries_col(utils_conn, dataset_name)
+    col_info = get_column_detailed_info(
+        datasets_conn, dataset_name, column_name, timeseries_col
+    )
+    return {"column": col_info, "timeseries_col": timeseries_col}
+
+
+class TimeseriesColumn(BaseModel):
+    new_timeseries_col: str | None
+
+
+@router.put("/{dataset_name}/update-timeseries-col")
+async def route_update_timeseries_col(dataset_name: str, body: TimeseriesColumn):
+    utils_conn = create_connection(os.path.join(APP_DATA_PATH, DB_DATASETS_UTIL))
+    success = update_timeseries_col(utils_conn, dataset_name, body.new_timeseries_col)
+    if success:
+        return {"message": "Update successful"}
+    else:
+        raise HTTPException(status_code=400, detail="Update failed")
 
 
 @router.get("/all-columns")
