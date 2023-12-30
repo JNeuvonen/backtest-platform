2d1e984175bab4d6bbc4d67d4e523856a94e2450
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-30 13:57:59 +0200

    Update sql_statements.py

--- pyserver/src/sql_statements.py
+++ pyserver/src/sql_statements.py
@@ -1,10 +1,11 @@
-from constants import DATASET_UTIL_TABLE_NAME, DatasetUtilsColumns
+from db import DatasetUtils
 
+DB_UTIL_COLS = DatasetUtils.Columns
 
 CREATE_DATASET_UTILS_TABLE = f"""
-    CREATE TABLE IF NOT EXISTS {DATASET_UTIL_TABLE_NAME} (
+    CREATE TABLE IF NOT EXISTS {DatasetUtils.TABLE_NAME} (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        {DatasetUtilsColumns.DATASET_NAME.value} TEXT NOT NULL,
-        {DatasetUtilsColumns.TIMESERIES_COLUMN.value} TEXT
+        {DB_UTIL_COLS.DATASET_NAME.value} TEXT NOT NULL,
+        {DB_UTIL_COLS.TIMESERIES_COLUMN.value} TEXT
     );
 """
