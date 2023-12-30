f8927c28b986a882a6a674d87c045424fb319ade
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-30 17:08:55 +0200

    Update useMessageListener.tsx

--- client/hooks/useMessageListener.tsx
+++ client/hooks/useMessageListener.tsx
@@ -9,5 +9,5 @@ export const useMessageListener = ({ messageName, messageCallback }: Props) => {
   useEffect(() => {
     window.addEventListener(messageName, messageCallback);
     return () => window.removeEventListener(messageName, messageCallback);
-  }, []);
+  }, [messageCallback, messageName]);
 };
