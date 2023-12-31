8a1deb62506f9b631eddc4115c3e7da9ad86d18d
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2023-12-31 11:57:22 +0200

    Create useKeyListener.tsx

---
+++ client/hooks/useKeyListener.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+interface Props {
+  eventAction: (event: KeyboardEvent) => void;
+  addToDom?: boolean;
+}
+
+export const useKeyListener = ({ eventAction, addToDom }: Props) => {
+  useEffect(() => {
+    if (addToDom) {
+      window.addEventListener("keydown", eventAction);
+    }
+    return () => {
+      window.removeEventListener("keydown", eventAction);
+    };
+  }, [addToDom, eventAction]);
+};
