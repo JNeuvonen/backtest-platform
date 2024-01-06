148a4214b5fed1d9a945ffb658054fe9eb3f7595
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-06 17:54:29 +0200

    Create useForceUpdate.tsx

---
+++ client/hooks/useForceUpdate.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const useForceUpdate = () => {
+  const [, setTick] = useState(0);
+  const update = () => setTick((tick) => tick + 1);
+  return update;
+};
