ddbd6d4fd8587bf4f7c19564090b93c6d3150ffd
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-07 14:40:20 +0200

    Update tauri.ts

--- client/utils/tauri.ts
+++ client/utils/tauri.ts
@@ -1,9 +1,19 @@
 import { invoke } from "@tauri-apps/api";
 import { TAURI_COMMANDS } from "./constants";
 
-export async function retrieveEnvVar(envVar: string) {
+export async function fetchEnvVar(envVar: string) {
   try {
     const url = await invoke(TAURI_COMMANDS.fetch_env, { key: envVar });
+    return url;
+  } catch (error) {
+    console.error("Error getting backend URL:", error);
+    return "";
+  }
+}
+
+export async function fetchPlatform() {
+  try {
+    const url = await invoke(TAURI_COMMANDS.fetch_platform);
     return url;
   } catch (error) {
     console.error("Error getting backend URL:", error);
