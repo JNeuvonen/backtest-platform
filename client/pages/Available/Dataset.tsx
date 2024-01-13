3306294520cd1cf0771f4299d7c599b8108b2b3f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-13 16:43:38 +0200

    Update Dataset.tsx

--- client/pages/Available/Dataset.tsx
+++ client/pages/Available/Dataset.tsx
@@ -132,6 +132,7 @@ export const DatasetDetailPage = () => {
         <CombineDataset
           baseDataset={datasetName}
           baseDatasetColumns={columns}
+          refetchParent={refetch}
         />
       </ChakraModal>
       <Box
