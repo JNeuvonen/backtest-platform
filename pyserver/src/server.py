7eafbd5fa3fa76c1a96f733c82860cdb959db233
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-13 15:55:49 +0200

    Update server.py

--- pyserver/src/server.py
+++ pyserver/src/server.py
@@ -1,18 +1,13 @@
 import os
-import sqlite3
 from contextlib import asynccontextmanager
 import uvicorn
 
-from fastapi import FastAPI, Response, status, HTTPException, Path
+from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from db import (
     DatasetUtils,
-    create_connection,
     exec_sql,
-    get_column_names,
-    get_tables,
 )
-from constants import DB_DATASETS
 from route_binance import router as binance_router
 from route_datasets import router as datasets_router
 from sql_statements import CREATE_DATASET_UTILS_TABLE
@@ -44,49 +39,11 @@ app.add_middleware(
 APP_DATA_PATH = os.getenv("APP_DATA_PATH", "")
 
 
-def get_path(relative_to_app_data: str):
-    return os.path.join(APP_DATA_PATH, relative_to_app_data)
-
-
 @app.get("/", response_class=Response)
 def read_root():
     return Response(
         content="Hello World", media_type="text/plain", status_code=status.HTTP_200_OK
     )
-
-
-@app.get("/tables")
-async def read_tables():
-    db_path = get_path(DB_DATASETS)
-    try:
-        db = create_connection(db_path)
-        tables = get_tables(db)
-        db.close()
-        return {"tables": tables}
-    except sqlite3.Error as e:
-        error_detail = {"error": str(e), "db_path": db_path}
-        raise HTTPException(status_code=500, detail=error_detail) from e
-
-
-@app.get("/tables/{table_name}/columns")
-def read_table_columns(table_name: str = Path(..., title="The name of the table")):
-    db_path = os.path.join(APP_DATA_PATH, DB_DATASETS)
-    db_connection = create_connection(db_path)
-
-    try:
-        # Get the list of column names from the table
-        columns = get_column_names(db_connection, table_name)
-    except sqlite3.Error as e:
-        db_connection.close()
-        error_detail = {
-            "error": str(e),
-            "app_data_path": APP_DATA_PATH,
-            "db_path": db_path,
-        }
-        raise HTTPException(status_code=500, detail=error_detail) from e
-
-    db_connection.close()
-    return {"table": table_name, "columns": columns}
 
 
 if __name__ == "__main__":
