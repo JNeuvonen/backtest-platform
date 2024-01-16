cbc93b8bf8da3c8b4e28fde466ed13a63c46ddd6
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 14:32:42 +0200

    Update t_context.py

--- tests/t_context.py
+++ tests/t_context.py
@@ -1,15 +1,16 @@
 import sys
 from contextlib import contextmanager
+from tests.t_conf import SERVER_SOURCE_DIR
 
 from tests.t_constants import FixturePaths
 
-sys.path.append("pyserver/src")
+sys.path.append(SERVER_SOURCE_DIR)
 
 from config import append_app_data_path
 
 
 @contextmanager
-def binance_file(filename):
+def t_binance_file(filename):
     try:
         file = open(append_app_data_path(FixturePaths.BINANCE.format(filename)), "rb")
         yield file
