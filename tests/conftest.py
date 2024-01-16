0cc319db04cbacfe840c0d69eab2d2c84512b247
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 14:32:32 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ import pandas as pd
 import pytest
 import time
 import sys
+from tests.t_conf import SERVER_SOURCE_DIR
 
 from tests.t_constants import Constants, FixturePaths
 
-sys.path.append("pyserver/src")
+sys.path.append(SERVER_SOURCE_DIR)
 
 import server
 from utils import rm_file
@@ -17,21 +18,21 @@ from constants import LOG_FILE, DB_DATASETS
 from sql_statements import CREATE_DATASET_UTILS_TABLE
 
 
-def binance_path_to_dataset_name(binance_path: str):
+def t_binance_path_to_dataset_name(binance_path: str):
     parts = binance_path.split("-")
     return parts[0].lower() + "_" + parts[1]
 
 
-def read_binance_df(dataset_name):
+def t_read_binance_df(dataset_name):
     return pd.read_csv(append_app_data_path(FixturePaths.BINANCE.format(dataset_name)))
 
 
-def init_server():
+def t_init_server():
     os.environ["APP_DATA_PATH"] = Constants.TESTS_FOLDER
     server.run()
 
 
-def rm_db():
+def t_rm_db():
     rm_file(append_app_data_path(DatasetUtils.DB_PATH))
     rm_file(append_app_data_path(LOG_FILE))
     rm_file(append_app_data_path(DB_DATASETS))
@@ -39,15 +40,15 @@ def rm_db():
 
 @pytest.fixture
 def cleanup_db():
-    rm_db()
+    t_rm_db()
     exec_sql(DatasetUtils.get_path(), CREATE_DATASET_UTILS_TABLE)
     yield
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_test_environment():
-    rm_db()
-    process = multiprocessing.Process(target=init_server)
+    t_rm_db()
+    process = multiprocessing.Process(target=t_init_server)
     process.start()
     time.sleep(3)
     yield
