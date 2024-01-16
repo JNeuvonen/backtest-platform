7eefe10140f89270b1bd17bec388b17e89732c79
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 13:34:22 +0200

    Update t_constants.py

--- tests/t_constants.py
+++ tests/t_constants.py
@@ -1,33 +1,53 @@
+import sys
 from tests.t_utils import fetch_json
+
+sys.path.append("pyserver/src")
+from server import Routers
+from route_datasets import RoutePaths
 
 
 class Constants:
     TESTS_FOLDER = "/tests"
 
 
+class MockDataset:
+    def __init__(self, path: str, dataset_name: str) -> None:
+        self.path = path
+        self.name = dataset_name
+
+
 class BinanceData:
-    BTCUSDT_1H_2023_06 = "BTCUSDT-1h-2023-06.csv"
-    DOGEUSDT_1H_2023_06 = "DOGEUSDT-1h-2023-06.csv"
-    ETHBTC_1H_2023_06 = "ETHBTC-1h-2023-06.csv"
-    ETHUSDT_1H_2023_06 = "ETHUSDT-1h-2023-06.csv"
+    BTCUSDT_1H_2023_06 = MockDataset("BTCUSDT-1h-2023-06.csv", "btcusdt_1h")
+    DOGEUSDT_1H_2023_06 = MockDataset("DOGEUSDT-1h-2023-06.csv", "dogeusdt_1h")
+    ETHBTC_1H_2023_06 = MockDataset("ETHBTC-1h-2023-06.csv", "ethbtc_1h")
+    ETHUSDT_1H_2023_06 = MockDataset("ETHUSDT-1h-2023-06.csv", "ethusdt_1h")
+
+
+class FixturePaths:
+    BINANCE = "fixtures/binance/{}"
 
 
 class URL:
     BASE_URL = "http://localhost:8000"
 
-    class Datasets:
-        ROUTE = "/dataset"
-        TABLES = "/tables"
+    @classmethod
+    def _datasets_route(cls):
+        return cls.BASE_URL + Routers.DATASET
 
-        @staticmethod
-        def get_tables_url():
-            return URL.BASE_URL + URL.Datasets.ROUTE + URL.Datasets.TABLES
+    @classmethod
+    def get_tables(cls):
+        return cls._datasets_route() + RoutePaths.ALL_TABLES
+
+    @classmethod
+    def get_upload_dataset_url(cls, table_name: str):
+        return (
+            cls._datasets_route()
+            + RoutePaths.UPLOAD_TIMESERIES_DATA
+            + f"?dataset_name={table_name}"
+        )
 
 
 class FetchData:
     @staticmethod
     def get_tables():
-        return fetch_json(URL.Datasets.get_tables_url())
-
-
-BINANCE_FIXTURES_PATH = "fixtures/binance/{}"
+        return fetch_json(URL.get_tables())
