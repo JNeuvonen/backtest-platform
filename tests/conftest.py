2afdbedfe7cf352a12b9c961ba9d0d39c64b14e2
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 14:54:12 +0200

    Add kill port to test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import os
+import subprocess
 import multiprocessing
 import pandas as pd
+from pandas.compat import platform
 import pytest
 import time
 import sys
@@ -45,8 +47,46 @@ def cleanup_db():
     yield
 
 
+def t_kill_port_with_npx(port):
+    try:
+        subprocess.run(
+            ["npx", "--version"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        subprocess.run(["npx", "kill-port", str(port)], check=True)
+        print(f"Port {port} killed using npx kill-port.")
+    except subprocess.CalledProcessError:
+        try:
+            if platform.system() == "Windows":
+                command = f"netstat -ano | findstr :{port}"
+                lines = (
+                    subprocess.check_output(command, shell=True).decode().splitlines()
+                )
+                for line in lines:
+                    pid = line.strip().split()[-1]
+                    subprocess.call(f"taskkill /F /PID {pid}", shell=True)
+            elif platform.system() == "Darwin":
+                command = f"lsof -i tcp:{port} | grep LISTEN"
+                lines = (
+                    subprocess.check_output(command, shell=True).decode().splitlines()
+                )
+                for line in lines:
+                    pid = line.split()[1]
+                    os.kill(int(pid), 9)
+            else:
+                command = f"fuser -k {port}/tcp"
+                subprocess.call(command, shell=True)
+
+        except Exception:
+            pass
+
+
 @pytest.fixture(scope="session", autouse=True)
 def setup_test_environment():
+    t_kill_port_with_npx(8000)
     t_rm_db()
     process = multiprocessing.Process(target=t_init_server)
     process.start()
