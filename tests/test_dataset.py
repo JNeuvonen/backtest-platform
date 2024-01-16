24a2fb4f41fb75acca1b51c21931eca43ec11b27
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 13:34:27 +0200

    Update test_dataset.py

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,13 +1,11 @@
-import json
 import pytest
 import sys
 
 import requests
 from tests.conftest import (
-    binance_path_to_dataset_name,
     read_binance_df,
 )
-from tests.t_constants import BinanceData, FetchData
+from tests.t_constants import URL, BinanceData, FetchData
 from tests.t_context import binance_file
 
 sys.path.append("pyserver/src")
@@ -17,8 +15,8 @@ from utils import add_to_datasets_db
 @pytest.mark.acceptance
 def test_setup_sanity(cleanup_db):
     _ = cleanup_db
-    df = read_binance_df(BinanceData.BTCUSDT_1H_2023_06)
-    add_to_datasets_db(df, binance_path_to_dataset_name(BinanceData.BTCUSDT_1H_2023_06))
+    df = read_binance_df(BinanceData.BTCUSDT_1H_2023_06.path)
+    add_to_datasets_db(df, BinanceData.BTCUSDT_1H_2023_06.name)
     tables = FetchData.get_tables()
     assert len(tables) == 1
 
@@ -26,14 +24,12 @@ def test_setup_sanity(cleanup_db):
 @pytest.mark.acceptance
 def test_upload_timeseries_data(cleanup_db):
     _ = cleanup_db
-    url = "http://localhost:8000/dataset/upload-timeseries-data?dataset_name=btcusdt_1h"
-    with binance_file(BinanceData.BTCUSDT_1H_2023_06) as file:
+    with binance_file(BinanceData.BTCUSDT_1H_2023_06.path) as file:
         response = requests.post(
-            url,
+            URL.get_upload_dataset_url("btcusdt_1h"),
             files={"file": ("file.csv", file)},
         )
-    result = response.json()
 
+    assert response.status_code == 200
     tables = FetchData.get_tables()
     assert len(tables) == 1
-    print("Hello world")
