22890ed989d3677731a794ccc062dd73beb7399a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-16 14:32:45 +0200

    Update t_utils.py

--- tests/t_utils.py
+++ tests/t_utils.py
@@ -1,7 +1,23 @@
 import requests
 
+from tests.t_constants import URL
 
-def fetch_json(url):
+
+def t_fetch_json(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
+
+
+def t_get_timeseries_col(table_item):
+    return table_item["timeseries_col"]
+
+
+def t_generate_big_dataset(data):
+    return
+
+
+class FetchData:
+    @staticmethod
+    def get_tables():
+        return t_fetch_json(URL.t_get_tables())["tables"]
