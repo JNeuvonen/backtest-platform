0f2b0b1d529fe1c356f569853d4a3360eff93c86
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-18 10:00:59 +0200

    Update Routes.tsx

--- client/Routes.tsx
+++ client/Routes.tsx
@@ -6,6 +6,7 @@ import { BrowseDatasetsPage } from "./pages/Datasets";
 import { DatasetIndex } from "./pages/dataset";
 import { DatasetInfoPage } from "./pages/dataset/info";
 import { DatasetEditorPage } from "./pages/dataset/editor";
+import { DatasetColumnInfoPage } from "./pages/dataset/column";
 
 export const AppRoutes = () => {
   return (
@@ -15,6 +16,11 @@ export const AppRoutes = () => {
       <Route path={PATHS.datasets.dataset} element={<DatasetIndex />}>
         <Route path={PATHS.datasets.info} element={<DatasetInfoPage />} />
         <Route path={PATHS.datasets.editor} element={<DatasetEditorPage />} />
+        <Route path={PATHS.datasets.editor} element={<DatasetEditorPage />} />
+        <Route
+          path={PATHS.datasets.column}
+          element={<DatasetColumnInfoPage />}
+        />
       </Route>
       <Route path={PATHS.simulate.path} element={<Home />} />
     </Routes>
