914e2de7591589ec046a228bc9f1a47ea24b86aa
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-18 11:00:41 +0200

    Update usePathParams.tsx

--- client/hooks/usePathParams.tsx
+++ client/hooks/usePathParams.tsx
@@ -1,15 +1,17 @@
 import { useParams } from "react-router-dom";
 
-interface Props {
-  key: string;
-}
-
-type PathParams = {
-  [key: string]: string;
+type PathParams<T = {}> = {
+  [key in keyof T]: string;
 };
 
-export const usePathParams = ({ key }: Props) => {
-  const params = useParams<PathParams>();
-  const item = params[key.replace(":", "")];
-  return item ?? "";
+export const usePathParams = <T extends {} = {}>() => {
+  const params = useParams<PathParams<T>>();
+
+  const transformedParams = Object.keys(params).reduce((acc, key) => {
+    const newKey = key.replace(":", "") as keyof T;
+    acc[newKey] = params[key];
+    return acc;
+  }, {} as PathParams<T>);
+
+  return transformedParams;
 };
