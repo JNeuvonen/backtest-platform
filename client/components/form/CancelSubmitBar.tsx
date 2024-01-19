24f0659df7a615eae12d78fbec0f84cc5724c8a9
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-19 15:46:42 +0200

    Update CancelSubmitBar.tsx

--- client/components/form/CancelSubmitBar.tsx
+++ client/components/form/CancelSubmitBar.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@chakra-ui/react";
 import React, { CSSProperties } from "react";
 import { BUTTON_VARIANTS } from "../../theme";
+import { useAppContext } from "../../context/app";
 
 interface Props {
   cancelText?: string;
   submitText?: string;
   submitDisabled?: boolean;
+  hideCancel?: boolean;
   cancelCallback?: () => void;
   submitCallback?: () => void;
   style?: CSSProperties;
+  mode?: "STICKY-BOTTOM" | "";
 }
 
 export const FormSubmitBar = ({
@@ -18,9 +21,21 @@ export const FormSubmitBar = ({
   submitText = "Submit",
   submitCallback,
   style = {},
+  mode = "",
 }: Props) => {
+  const { contentIndentPx } = useAppContext();
+  const stickyBottomStyle: CSSProperties = {
+    position: "fixed",
+    bottom: 20,
+    width: `calc(100% - ${contentIndentPx + 46}px)`,
+  };
+
+  // Combine the styles based on the mode
+  const combinedStyle =
+    mode === "STICKY-BOTTOM" ? { ...style, ...stickyBottomStyle } : style;
+
   return (
-    <div className="form-submit-bar" style={style}>
+    <div className="form-submit-bar" style={combinedStyle}>
       <Button variant={BUTTON_VARIANTS.nofill} onClick={cancelCallback}>
         {cancelText}
       </Button>
