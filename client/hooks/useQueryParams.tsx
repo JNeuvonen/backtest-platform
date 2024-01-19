2866ca1900733dd95bbc0217aa166547a30a9df0
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-19 12:33:04 +0200

    Create useQueryParams.tsx

---
+++ client/hooks/useQueryParams.tsx
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom";
+
+type QueryParams<T = {}> = {
+  [key in keyof T]: string | undefined;
+};
+
+const useQueryParams = <T extends QueryParams<T> = {}>() => {
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+
+  const params: QueryParams<T> = {} as QueryParams<T>;
+  queryParams.forEach((value, key) => {
+    (params as any)[key] = value;
+  });
+
+  return params as QueryParams<T>;
+};
+
+export default useQueryParams;
