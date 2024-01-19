d49ad55cf19d9f73e617622eba2ba37393df197a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-19 14:46:33 +0200

    Update RunPythonOnAllCols.tsx

--- client/components/RunPythonOnAllCols.tsx
+++ client/components/RunPythonOnAllCols.tsx
@@ -3,6 +3,7 @@ import { OnMount } from "@monaco-editor/react";
 import { PythonEditor } from "./PythonEditor";
 import { ChakraSelect } from "./chakra/select";
 import { DOM_IDS, NULL_FILL_STRATEGIES } from "../utils/constants";
+import { CodeEditor } from "./CodeInput";
 
 interface Props {
   code: string;
@@ -27,26 +28,7 @@ export const RunPythonOnAllCols = ({ code, setCode }: Props) => {
         id={DOM_IDS.select_null_fill_strat}
         defaultValueIndex={3}
       />
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          marginTop: "16px",
-        }}
-      >
-        <PythonEditor
-          code={code}
-          onChange={handleCodeChange}
-          editorMount={handleEditorDidMount}
-          height={"400px"}
-          containerStyles={{ width: "65%", height: "100%" }}
-          fontSize={13}
-        />
-
-        <div>Code presets will come here</div>
-      </div>
+      <CodeEditor code={code} setCode={setCode} />
     </div>
   );
 };
