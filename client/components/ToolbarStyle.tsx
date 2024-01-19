6c6fc69f5b124d76f6eb72c10abeb6d6920bde0d
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-19 12:33:03 +0200

    Create ToolbarStyle.tsx

---
+++ client/components/ToolbarStyle.tsx
@@ -0,0 +1,14 @@
+import React, { CSSProperties } from "react";
+interface Props {
+  children: React.ReactNode[];
+  style?: CSSProperties;
+}
+export const ToolBarStyle = ({ children, style }: Props) => {
+  return (
+    <div
+      style={{ display: "flex", alignItems: "center", gap: "8px", ...style }}
+    >
+      {children}
+    </div>
+  );
+};
