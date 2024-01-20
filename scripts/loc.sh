34b4db0da671d80c68e31a22a0df012260b2980c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-20 21:40:42 +0200

    Update loc.sh

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-#fun script for counting lines of .py, .rs, .ts and .tsx
-find . -type f \( -name '*.py' -o -name '*.rs' -o -name '*.ts' -o -name '*.tsx' \) -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/build/*' | xargs wc -l
+
+py_file_count=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/build/*' | wc -l)
+ts_file_count=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' | wc -l)
+total_file_count=$((py_file_count + ts_file_count))
+
+py_lines=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/build/*' | xargs cat | wc -l)
+ts_lines=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' | xargs cat | wc -l)
+total_line_count=$((py_lines + ts_lines))
+
+echo "Line Counts:"
+echo "------------"
+echo "Python line count: $py_lines"
+echo "TypeScript line count: $ts_lines"
+echo "Total line count: $total_line_count"
+
+echo ""
+echo "File Counts:"
+echo "------------"
+echo "Python file count: $py_file_count"
+echo "TypeScript file count: $ts_file_count"
+echo "Total file count: $total_file_count"
+
+
+
