cb1d366ca4bc49f27be50c968c8b7f067270a784
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-20 15:38:19 +0200

    Update request_types.py

--- pyserver/src/request_types.py
+++ pyserver/src/request_types.py
@@ -2,7 +2,7 @@ from typing import List
 from pydantic import BaseModel
 
 
-class ModelData(BaseModel):
+class BodyModelData(BaseModel):
     name: str
     target_col: str
     drop_cols: List[str]
@@ -10,3 +10,21 @@ class ModelData(BaseModel):
     model: str
     hyper_params_and_optimizer_code: str
     validation_split: List[int]
+
+
+class BodyExecPython(BaseModel):
+    code: str
+    null_fill_strategy: str = "NONE"
+
+
+class BodyUpdateTimeseriesCol(BaseModel):
+    new_timeseries_col: str
+
+
+class BodyUpdateDatasetName(BaseModel):
+    new_dataset_name: str
+
+
+class BodyRenameColumn(BaseModel):
+    old_col_name: str
+    new_col_name: str
