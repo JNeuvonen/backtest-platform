262f6e2e98160db2601c6bb8b5d658891813f61c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-20 12:40:55 +0200

    Update index.tsx

--- client/pages/data/model/index.tsx
+++ client/pages/data/model/index.tsx
@@ -1,4 +1,36 @@
 import React from "react";
+import usePath from "../../../hooks/usePath";
+import { ChakraModal } from "../../../components/chakra/modal";
+import { useModal } from "../../../hooks/useOpen";
+import { Button } from "@chakra-ui/react";
+import { DatasetModelCreatePage } from "./create";
+import { FormSubmitBar } from "../../../components/form/CancelSubmitBar";
+
+const TAB_LABELS = ["Available", "Create", "Train"];
 export const DatasetModelIndex = () => {
-  return <div>dataset model index</div>;
+  const { path } = usePath();
+
+  const createModelModal = useModal();
+
+  const createNewModel = async () => {};
+
+  return (
+    <div>
+      <ChakraModal
+        {...createModelModal}
+        title="Create Model"
+        modalContentStyle={{ maxWidth: "80%" }}
+        footerContent={
+          <FormSubmitBar
+            cancelCallback={createModelModal.onClose}
+            submitCallback={createNewModel}
+          />
+        }
+      >
+        <DatasetModelCreatePage />
+      </ChakraModal>
+      <Button onClick={createModelModal.onOpen}>Create</Button>
+      <div>no models available</div>
+    </div>
+  );
 };
