2e2f7a9c82dd22f934fffc3ae70ffe1c236ed613
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-21 14:00:36 +0200

    Create CreateTrainJobForm.tsx

---
+++ client/components/CreateTrainJobForm.tsx
@@ -0,0 +1,156 @@
+import React from "react";
+import { useModelQuery } from "../clients/queries/queries";
+import { usePathParams } from "../hooks/usePathParams";
+import {
+  Button,
+  Checkbox,
+  NumberInput,
+  NumberInputField,
+  Spinner,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+import { WithLabel } from "./form/WithLabel";
+import { CodeEditor } from "./CodeEditor";
+import { CodeHelper } from "../utils/constants";
+import { FormSubmitBar } from "./form/FormSubmitBar";
+
+interface RouteParams {
+  datasetName: string;
+  modelName: string;
+}
+
+interface Props {
+  onClose?: () => void;
+}
+
+interface TrainJobForm {
+  numEpochs: number;
+  saveModelEveryEpoch: boolean;
+  backtestOnValidationSet: boolean;
+  enterTradeCriteria: string;
+  exitTradeCriteria: string;
+}
+
+const generateEnterTradeExample = () => {
+  const code = new CodeHelper();
+  code.appendLine("last_prediction = get_prediction()");
+  code.appendLine("return last_prediction > 1.01");
+  return code.get();
+};
+
+const generateExitTradeExample = () => {
+  const code = new CodeHelper();
+  code.appendLine("last_prediction = get_prediction()");
+  code.appendLine("return last_prediction < 0.99");
+  return code.get();
+};
+
+export const CreateTrainJobForm = ({ onClose }: Props) => {
+  const { modelName } = usePathParams<RouteParams>();
+  const { data } = useModelQuery(modelName);
+
+  if (!data) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+
+  const onSubmit = async () => {};
+
+  return (
+    <div>
+      <Formik
+        onSubmit={(values: TrainJobForm, actions) => {
+          console.log(values);
+        }}
+        initialValues={{
+          numEpochs: 100,
+          saveModelEveryEpoch: false,
+          backtestOnValidationSet: true,
+          enterTradeCriteria: generateEnterTradeExample(),
+          exitTradeCriteria: generateExitTradeExample(),
+        }}
+      >
+        {({ values }) => (
+          <Form>
+            <WithLabel
+              label="Number of epochs"
+              containerStyles={{ maxWidth: "250px" }}
+            >
+              <Field name="numEpochs">
+                {({ field }) => (
+                  <NumberInput step={5} min={10} {...field}>
+                    <NumberInputField {...field} />
+                  </NumberInput>
+                )}
+              </Field>
+            </WithLabel>
+
+            <div style={{ marginTop: "16px" }}>
+              <Field type="checkbox" name="saveModelEveryEpoch">
+                {({ field }) => (
+                  <Checkbox {...field} isChecked={field.value}>
+                    Save model after every epoch
+                  </Checkbox>
+                )}
+              </Field>
+            </div>
+            <div style={{ marginTop: "4px" }}>
+              <Field type="checkbox" name="backtestOnValidationSet">
+                {({ field }) => (
+                  <Checkbox {...field} isChecked={field.value}>
+                    Backtest on validation set
+                  </Checkbox>
+                )}
+              </Field>
+            </div>
+
+            {values.backtestOnValidationSet && (
+              <>
+                <WithLabel
+                  label="Enter trade criteria"
+                  containerStyles={{ marginTop: "16px" }}
+                >
+                  <Field name="enterTradeCriteria">
+                    {({ field, form }) => (
+                      <CodeEditor
+                        code={field.value}
+                        setCode={(code) => form.setFieldValue(field.name, code)}
+                        fontSize={15}
+                        height={"200px"}
+                        autoFocus={false}
+                      />
+                    )}
+                  </Field>
+                </WithLabel>
+                <WithLabel
+                  label="Exit trade criteria"
+                  containerStyles={{ marginTop: "16px" }}
+                >
+                  <Field name="exitTradeCriteria">
+                    {({ field, form }) => (
+                      <CodeEditor
+                        code={field.value}
+                        setCode={(code) => form.setFieldValue(field.name, code)}
+                        fontSize={15}
+                        height={"200px"}
+                        autoFocus={false}
+                      />
+                    )}
+                  </Field>
+                </WithLabel>
+              </>
+            )}
+
+            <FormSubmitBar
+              style={{ marginTop: "16px" }}
+              cancelCallback={onClose}
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
