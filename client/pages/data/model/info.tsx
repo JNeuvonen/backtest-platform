7170064e9cded9c6068e1119a5b381286113c708
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-21 11:39:56 +0200

    Update info.tsx

--- client/pages/data/model/info.tsx
+++ client/pages/data/model/info.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { usePathParams } from "../../../hooks/usePathParams";
+import Title from "../../../components/typography/Title";
+import { useModelQuery } from "../../../clients/queries/queries";
+import { Spinner } from "@chakra-ui/react";
+import { CodeEditor } from "../../../components/CodeEditor";
+import { WithLabel } from "../../../components/form/WithLabel";
+import { formatValidationSplit } from "../../../utils/content";
+import { ValidationSplitSlider } from "../../../components/ValidationSplitSlider";
 
 interface RouteParams {
   datasetName: string;
@@ -7,6 +14,41 @@ interface RouteParams {
 }
 
 export const ModelInfoPage = () => {
-  const { modelName, datasetName } = usePathParams<RouteParams>();
-  return <div>model info page</div>;
+  const { modelName } = usePathParams<RouteParams>();
+  const { data } = useModelQuery(modelName);
+
+  if (!data) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Title>{modelName}</Title>
+      <CodeEditor
+        code={data.model}
+        readOnly={true}
+        fontSize={15}
+        disableCodePresets={true}
+        codeContainerStyles={{ width: "100%", marginTop: "16px" }}
+        label={"Model"}
+      />
+      <CodeEditor
+        code={data.hyper_params_and_optimizer_code}
+        readOnly={true}
+        fontSize={15}
+        disableCodePresets={true}
+        codeContainerStyles={{ width: "100%", marginTop: "16px" }}
+        label={"Hyper params and optimizer"}
+        height="200px"
+      />
+      <ValidationSplitSlider
+        sliderValue={data.validation_split}
+        containerStyle={{ width: "250px", marginTop: "16px" }}
+      />
+    </div>
+  );
 };
