4bf4539e2bc5a324bc1c21564207ba48648e1116
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-22 10:02:23 +0200

    Update route_model.py

--- pyserver/src/route_model.py
+++ pyserver/src/route_model.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from context import HttpResponseContext
 from db import DatasetUtils
+from pyserver.src.request_types import BodyCreateTrain
 
 
 router = APIRouter()
@@ -9,6 +10,7 @@ router = APIRouter()
 
 class RoutePaths:
     FETCH_MODEL = "/{model_name}"
+    CREATE_TRAIN_JOB = "/{model_name}/create-train"
 
 
 @router.get(RoutePaths.FETCH_MODEL)
@@ -16,3 +18,10 @@ async def route_fetch_model(model_name: str):
     with HttpResponseContext():
         model = DatasetUtils.fetch_model_by_name(model_name)
         return {"model": model}
+
+
+@router.post(RoutePaths.CREATE_TRAIN_JOB)
+async def route_create_train_job(model_name: str, body: BodyCreateTrain):
+    with HttpResponseContext():
+        DatasetUtils.create_train_job(model_name, body)
+        return {"message": "OK"}
