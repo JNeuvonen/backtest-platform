4c3a904b378e10776cd7901720e71c53eaeb9de0
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-22 10:02:20 +0200

    Update request_types.py

--- pyserver/src/request_types.py
+++ pyserver/src/request_types.py
@@ -28,3 +28,11 @@ class BodyUpdateDatasetName(BaseModel):
 class BodyRenameColumn(BaseModel):
     old_col_name: str
     new_col_name: str
+
+
+class BodyCreateTrain(BaseModel):
+    num_epochs: int
+    save_model_after_every_epoch: bool
+    backtest_on_val_set: bool
+    enter_trade_criteria: str
+    exit_trade_criteria: str
