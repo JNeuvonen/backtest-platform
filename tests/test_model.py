a98dc38a5de1a855b4c6537fd908fcb74188282c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-22 10:02:31 +0200

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,10 +1,16 @@
 import pytest
+from tests.fixtures import create_train_job_basic
 from tests.t_constants import Constants, DatasetMetadata
 
-from tests.t_utils import Fetch
+from tests.t_utils import Fetch, Post
 
 
 @pytest.mark.acceptance
 def test_fetch_model_by_name(cleanup_db, create_basic_model: DatasetMetadata):
     model = Fetch.get_model_by_name(Constants.EXAMPLE_MODEL_NAME)
     assert model["name"] == Constants.EXAMPLE_MODEL_NAME
+
+
+@pytest.mark.acceptance
+def test_create_train_job(cleanup_db, create_basic_model: DatasetMetadata):
+    Post.create_train_job(Constants.EXAMPLE_MODEL_NAME, body=create_train_job_basic())
