23bbc4ee9def472bd3d026935485c6e8f2eb0c28
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-22 10:34:11 +0200

    Update route_model.py

--- pyserver/src/route_model.py
+++ pyserver/src/route_model.py
@@ -11,6 +11,7 @@ router = APIRouter()
 class RoutePaths:
     FETCH_MODEL = "/{model_name}"
     CREATE_TRAIN_JOB = "/{model_name}/create-train"
+    TRAIN_JOB_BY_ID = "/train/{id}"
 
 
 @router.get(RoutePaths.FETCH_MODEL)
@@ -23,5 +24,15 @@ async def route_fetch_model(model_name: str):
 @router.post(RoutePaths.CREATE_TRAIN_JOB)
 async def route_create_train_job(model_name: str, body: BodyCreateTrain):
     with HttpResponseContext():
-        DatasetUtils.create_train_job(model_name, body)
+        train_job_name = DatasetUtils.create_train_job(model_name, body)
+        train_job = DatasetUtils.get_train_job(
+            train_job_name, DatasetUtils.TrainJob.Cols.NAME
+        )
         return {"message": "OK"}
+
+
+@router.get(RoutePaths.TRAIN_JOB_BY_ID)
+async def route_fetch_train_job(id: int):
+    with HttpResponseContext():
+        train = DatasetUtils.get_train_job(id)
+        return {"train": train}
