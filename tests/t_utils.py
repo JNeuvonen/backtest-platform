d402a36a1bac8b9027f23f42639f554182b8c841
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-22 10:45:21 +0200

    Update t_utils.py

--- tests/t_utils.py
+++ tests/t_utils.py
@@ -185,26 +185,3 @@ class Put:
     def update_timeseries_col(dataset_name: str, body):
         with Req("put", URL.update_timeseries_col(dataset_name), json=body) as res:
             return res.json()
-
-
-class CodeHelper:
-    def __init__(self):
-        self.indent_level = 0
-        self.code = ""
-
-    def append_line(self, line: str):
-        new_line = "    " * self.indent_level + line + "\n"
-        self.code += new_line
-
-    def add_indent(self):
-        self.indent_level += 1
-
-    def reset_indent(self):
-        self.indent_level = 0
-
-    def reduce_indent(self):
-        if self.indent_level > 0:
-            self.indent_level -= 1
-
-    def get(self):
-        return self.code
