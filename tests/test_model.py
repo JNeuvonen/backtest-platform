05d4389bfa1decd4e9d43d59081e7182bf726c64
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-23 14:18:43 +0200

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,7 +8,7 @@ from tests.t_utils import Fetch, Post
 @pytest.mark.acceptance
 def test_fetch_model_by_name(cleanup_db, create_basic_model: DatasetMetadata):
     model = Fetch.get_model_by_name(Constants.EXAMPLE_MODEL_NAME)
-    assert model["name"] == Constants.EXAMPLE_MODEL_NAME
+    assert model["model_name"] == Constants.EXAMPLE_MODEL_NAME
 
 
 @pytest.mark.acceptance
