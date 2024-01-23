2aebcd1e689626370c6a085d27039e15c57ff138
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-23 14:43:29 +0200

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,5 @@
 import pytest
+from pyserver.src.orm import TrainJobQuery
 from tests.fixtures import create_train_job_basic
 from tests.t_constants import Constants, DatasetMetadata
 
@@ -14,3 +15,17 @@ def test_fetch_model_by_name(cleanup_db, create_basic_model: DatasetMetadata):
 @pytest.mark.acceptance
 def test_create_train_job(cleanup_db, create_basic_model: DatasetMetadata):
     Post.create_train_job(Constants.EXAMPLE_MODEL_NAME, body=create_train_job_basic())
+
+
+@pytest.mark.acceptance
+def test_toggling_is_training_status(cleanup_db, create_train_job):
+    train_job = create_train_job[2]
+    TrainJobQuery.set_training_status(train_job.id, True)
+    train_job_1 = TrainJobQuery.get_train_job(train_job.id)
+
+    assert train_job_1.is_training is True
+
+    TrainJobQuery.set_training_status(train_job.id, False)
+
+    train_job_2 = TrainJobQuery.get_train_job(train_job.id)
+    assert train_job_2.is_training is False
