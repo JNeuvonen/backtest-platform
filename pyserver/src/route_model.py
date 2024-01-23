829c781328e2a6cd189a48e02d94b3249d8b4b60
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-23 14:18:36 +0200

    Update route_model.py

--- pyserver/src/route_model.py
+++ pyserver/src/route_model.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from context import HttpResponseContext
-from db import DatasetUtils
 from code_gen import CodeGen
+from orm import ModelQuery, TrainJobQuery
 from request_types import BodyCreateTrain
 
 
@@ -18,19 +18,17 @@ class RoutePaths:
 @router.get(RoutePaths.FETCH_MODEL)
 async def route_fetch_model(model_name: str):
     with HttpResponseContext():
-        model = DatasetUtils.fetch_model_by_name(model_name)
+        model = ModelQuery.fetch_model_by_name(model_name)
         return {"model": model}
 
 
 @router.post(RoutePaths.CREATE_TRAIN_JOB)
 async def route_create_train_job(model_name: str, body: BodyCreateTrain):
     with HttpResponseContext():
-        train_job_name = DatasetUtils.create_train_job(model_name, body)
+        train_job_name = TrainJobQuery.create_train_job(model_name, body)
 
-        train_job = DatasetUtils.get_train_job(
-            train_job_name, DatasetUtils.TrainJob.Cols.NAME
-        )
-        model = DatasetUtils.fetch_model_by_name(model_name)
+        train_job = TrainJobQuery.get_train_job(train_job_name, "name")
+        model = ModelQuery.fetch_model_by_name(model_name)
 
         if train_job is None or model is None:
             # should never execute, just to make mypy happy
@@ -44,5 +42,5 @@ async def route_create_train_job(model_name: str, body: BodyCreateTrain):
 @router.get(RoutePaths.TRAIN_JOB_BY_ID)
 async def route_fetch_train_job(id: int):
     with HttpResponseContext():
-        train = DatasetUtils.get_train_job(id)
+        train = TrainJobQuery.get_train_job(id)
         return {"train": train}
