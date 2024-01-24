2ab55d17b4e9a5e4f632977d72a0587e2f7baa12
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-24 10:03:58 +0200

    Update CreateTrainJobForm.tsx

--- client/components/CreateTrainJobForm.tsx
+++ client/components/CreateTrainJobForm.tsx
@@ -7,12 +7,12 @@ import {
   NumberInput,
   NumberInputField,
   Spinner,
+  useToast,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 import { WithLabel } from "./form/WithLabel";
-import { CodeEditor } from "./CodeEditor";
-import { CodeHelper } from "../utils/constants";
 import { FormSubmitBar } from "./form/FormSubmitBar";
+import { createTrainJob } from "../clients/requests";
 
 interface RouteParams {
   datasetName: string;
@@ -23,33 +23,16 @@ interface Props {
   onClose?: () => void;
 }
 
-interface TrainJobForm {
+export interface TrainJobForm {
   numEpochs: number;
   saveModelEveryEpoch: boolean;
   backtestOnValidationSet: boolean;
-  enterTradeCriteria: string;
-  exitTradeCriteria: string;
 }
-
-const generateEnterTradeExample = () => {
-  const code = new CodeHelper();
-  code.appendLine("def get_enter_trade_criteria(prediction):");
-  code.addIndent();
-  code.appendLine("return prediction > 1.01");
-  return code.get();
-};
-
-const generateExitTradeExample = () => {
-  const code = new CodeHelper();
-  code.appendLine("def get_exit_trade_criteria(prediction):");
-  code.addIndent();
-  code.appendLine("return prediction < 0.99");
-  return code.get();
-};
 
 export const CreateTrainJobForm = ({ onClose }: Props) => {
   const { modelName } = usePathParams<RouteParams>();
   const { data } = useModelQuery(modelName);
+  const toast = useToast();
 
   if (!data) {
     return (
@@ -59,7 +42,23 @@ export const CreateTrainJobForm = ({ onClose }: Props) => {
     );
   }
 
-  const onSubmit = async (values: TrainJobForm) => {};
+  const onSubmit = async (values: TrainJobForm) => {
+    const res = await createTrainJob(modelName, {
+      num_epochs: values.numEpochs,
+      save_model_after_every_epoch: values.saveModelEveryEpoch,
+      backtest_on_val_set: values.backtestOnValidationSet,
+    });
+
+    if (res.status === 200) {
+      toast({
+        title: "Started training",
+        status: "info",
+        duration: 5000,
+        isClosable: true,
+      });
+      if (onClose) onClose();
+    }
+  };
 
   return (
     <div>
@@ -71,87 +70,36 @@ export const CreateTrainJobForm = ({ onClose }: Props) => {
           numEpochs: 100,
           saveModelEveryEpoch: false,
           backtestOnValidationSet: true,
-          enterTradeCriteria: generateEnterTradeExample(),
-          exitTradeCriteria: generateExitTradeExample(),
         }}
       >
-        {({ values }) => (
-          <Form>
-            <WithLabel
-              label="Number of epochs"
-              containerStyles={{ maxWidth: "250px" }}
-            >
-              <Field name="numEpochs">
-                {({ field }) => (
-                  <NumberInput step={5} min={10} {...field}>
-                    <NumberInputField {...field} />
-                  </NumberInput>
-                )}
-              </Field>
-            </WithLabel>
+        <Form>
+          <WithLabel
+            label="Number of epochs"
+            containerStyles={{ maxWidth: "250px" }}
+          >
+            <Field name="numEpochs">
+              {({ field }) => (
+                <NumberInput step={5} min={10} {...field}>
+                  <NumberInputField {...field} />
+                </NumberInput>
+              )}
+            </Field>
+          </WithLabel>
 
-            <div style={{ marginTop: "16px" }}>
-              <Field type="checkbox" name="saveModelEveryEpoch">
-                {({ field }) => (
-                  <Checkbox {...field} isChecked={field.value}>
-                    Save model after every epoch
-                  </Checkbox>
-                )}
-              </Field>
-            </div>
-            <div style={{ marginTop: "4px" }}>
-              <Field type="checkbox" name="backtestOnValidationSet">
-                {({ field }) => (
-                  <Checkbox {...field} isChecked={field.value}>
-                    Backtest on validation set
-                  </Checkbox>
-                )}
-              </Field>
-            </div>
-
-            {values.backtestOnValidationSet && (
-              <>
-                <WithLabel
-                  label="Enter trade criteria"
-                  containerStyles={{ marginTop: "16px" }}
-                >
-                  <Field name="enterTradeCriteria">
-                    {({ field, form }) => (
-                      <CodeEditor
-                        code={field.value}
-                        setCode={(code) => form.setFieldValue(field.name, code)}
-                        fontSize={15}
-                        height={"200px"}
-                        autoFocus={false}
-                      />
-                    )}
-                  </Field>
-                </WithLabel>
-                <WithLabel
-                  label="Exit trade criteria"
-                  containerStyles={{ marginTop: "16px" }}
-                >
-                  <Field name="exitTradeCriteria">
-                    {({ field, form }) => (
-                      <CodeEditor
-                        code={field.value}
-                        setCode={(code) => form.setFieldValue(field.name, code)}
-                        fontSize={15}
-                        height={"200px"}
-                        autoFocus={false}
-                      />
-                    )}
-                  </Field>
-                </WithLabel>
-              </>
-            )}
-
-            <FormSubmitBar
-              style={{ marginTop: "16px" }}
-              cancelCallback={onClose}
-            />
-          </Form>
-        )}
+          <div style={{ marginTop: "16px" }}>
+            <Field type="checkbox" name="saveModelEveryEpoch">
+              {({ field }) => (
+                <Checkbox {...field} isChecked={field.value}>
+                  Save model after every epoch
+                </Checkbox>
+              )}
+            </Field>
+          </div>
+          <FormSubmitBar
+            style={{ marginTop: "16px" }}
+            cancelCallback={onClose}
+          />
+        </Form>
       </Formik>
     </div>
   );
