f8de243f62c28f8382d4268a62c489f324b9b622
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-25 23:44:02 +0200

    Update Toolbar.tsx

--- client/components/Toolbar.tsx
+++ client/components/Toolbar.tsx
@@ -7,6 +7,8 @@ import { BUTTON_VARIANTS } from "../theme";
 import { MdOutlinePause } from "react-icons/md";
 import { roundNumberDropRemaining } from "../utils/number";
 import { stopTrain } from "../clients/requests";
+import { Link } from "react-router-dom";
+import { getTrainJobFromToolbar } from "../utils/navigate";
 
 const TrainingToolbar = () => {
   const {
@@ -40,15 +42,18 @@ const TrainingToolbar = () => {
         borderBottom: `1px solid ${COLOR_BG_PRIMARY_SHADE_TWO}`,
       }}
     >
-      <div>
+      <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
         Epoch: {epochsRan}/{maximumEpochs}, Train loss:{" "}
         {roundNumberDropRemaining(trainLosses[trainLosses.length - 1], 3)}, Val
         loss: {roundNumberDropRemaining(valLosses[valLosses.length - 1], 3)}
       </div>
-      <div>
+      <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+        <Link className="link-default" to={getTrainJobFromToolbar(trainJobId)}>
+          Details
+        </Link>
         <Button
           leftIcon={<MdOutlinePause />}
-          variant={BUTTON_VARIANTS.grey}
+          variant={BUTTON_VARIANTS.grey2}
           style={{ height: "28px" }}
           key={4}
           onClick={cancelTrain}
