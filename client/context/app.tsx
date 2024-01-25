6e1b1804c2a3c85798a1bbe735c94bc6910074ce
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-25 20:21:14 +0200

    Update app.tsx

--- client/context/app.tsx
+++ client/context/app.tsx
@@ -9,18 +9,24 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { LAYOUT, TAURI_COMMANDS } from "../utils/constants";
 
 export type Platform = "" | "macos" | "windows" | "linux";
+export type ToolbarMode = "TRAINING" | "";
 
 interface AppContextType {
   platform: Platform;
   contentIndentPx: number;
   innerSideNavWidth: number;
+  openTrainingToolbar: () => void;
+  parseEpochMessage: (msg: string) => void;
+  closeToolbar: () => void;
+  toolbarMode: ToolbarMode;
+  epochsRan: number;
+  maximumEpochs: number;
+  trainLosses: number[];
+  valLosses: number[];
+  epochTime: number;
 }
 
-export const AppContext = createContext<AppContextType>({
-  platform: "macos",
-  contentIndentPx: LAYOUT.side_nav_width_px,
-  innerSideNavWidth: LAYOUT.inner_side_nav_width_px,
-});
+export const AppContext = createContext<AppContextType>({} as AppContextType);
 
 interface AppProviderProps {
   children: ReactNode;
@@ -31,6 +37,12 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const [contentIndentPx] = useState(LAYOUT.side_nav_width_px);
   const [layoutPaddingPx] = useState(LAYOUT.layout_padding_px);
   const [innerSideNavWidth] = useState(LAYOUT.inner_side_nav_width_px);
+  const [toolbarMode, setToolbarMode] = useState<ToolbarMode>("");
+  const [epochsRan, setEpochsRan] = useState(0);
+  const [maximumEpochs, setMaximumEpochs] = useState(0);
+  const [trainLosses, setTrainLosses] = useState<number[]>([]);
+  const [valLosses, setValLosses] = useState<number[]>([]);
+  const [epochTime, setEpochTime] = useState<number>(0);
 
   useEffect(() => {
     const fetchAppData = async () => {
@@ -45,12 +57,56 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     fetchAppData();
   }, []);
 
+  const openTrainingToolbar = () => {
+    setToolbarMode("TRAINING");
+  };
+
+  const closeToolbar = () => {
+    setToolbarMode("");
+
+    setEpochsRan(0);
+    setEpochTime(0);
+    setMaximumEpochs(0);
+    setTrainLosses([]);
+    setValLosses([]);
+  };
+
+  const parseEpochMessage = (msg: string) => {
+    try {
+      const data = msg.split(":")[1].split("/");
+
+      const epochsRan = data[0];
+      const maxEpochs = data[1];
+      const trainLoss = data[2];
+      const valLoss = data[3];
+      const epochTime = data[4];
+
+      setEpochsRan(Number(epochsRan));
+      setMaximumEpochs(Number(maxEpochs));
+      setTrainLosses((prevTrainLosses) => [
+        ...prevTrainLosses,
+        Number(trainLoss),
+      ]);
+      setValLosses((prevValLosses) => [...prevValLosses, Number(valLoss)]);
+      setEpochTime(Number(epochTime));
+    } catch {}
+  };
+
   return (
     <AppContext.Provider
       value={{
         platform,
         contentIndentPx: contentIndentPx + layoutPaddingPx,
         innerSideNavWidth,
+        openTrainingToolbar,
+        closeToolbar,
+        toolbarMode,
+        parseEpochMessage,
+        epochsRan,
+        maximumEpochs,
+        trainLosses,
+        valLosses,
+        epochTime,
       }}
     >
       {children}
