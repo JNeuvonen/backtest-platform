eafd965d5d9a912ca5fba2db2c3e3ffd112bbae7
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-25 20:21:10 +0200

    Update index.tsx

--- client/components/layout/index.tsx
+++ client/components/layout/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { SideNav } from "./SideNav";
+import { useAppContext } from "../../context/App";
+import { LAYOUT } from "../../utils/constants";
+import { LayoutToolbar } from "../Toolbar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { toolbarMode } = useAppContext();
   return (
     <div className="layout">
       <SideNav />
-      <div className="layout__content">{children}</div>
+      <LayoutToolbar />
+      <div
+        className="layout__content"
+        id="layout__content"
+        style={{
+          paddingTop:
+            toolbarMode === "TRAINING" ? LAYOUT.training_toolbar_height : 0,
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 };
