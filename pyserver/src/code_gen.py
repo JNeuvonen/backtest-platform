255fd3eb59c5364dddc7e9badec17928899bd385
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-27 16:57:04 +0200

    Update code_gen.py

--- pyserver/src/code_gen.py
+++ pyserver/src/code_gen.py
@@ -1,8 +1,9 @@
-import threading
 from code_gen_template import TRAIN_TEMPLATE
 from config import is_testing
 from log import LogExceptionContext
-from orm import DatasetQuery, Model, TrainJob, ModelWeightsQuery
+from query_dataset import DatasetQuery
+from query_model import Model
+from query_trainjob import TrainJob
 from utils import convert_val_split_str_to_arr, global_symbols, run_in_thread
 
 
