51662432787239c4c6ff498be0852eb090621226
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-28 12:20:07 +0200

    Update fetch.ts

--- client/clients/fetch.ts
+++ client/clients/fetch.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { createStandaloneToast } from "@chakra-ui/react";
+const { toast } = createStandaloneToast();
+
 interface RequestProps {
   url: string;
   method: "GET" | "POST" | "PUT" | "DELETE";
   options?: RequestInit;
   payload?: object;
+  errorShouldNotifyUI?: boolean;
+  onCatchErrorMsg?: string;
+  errorDuration?: number;
 }
 
 export interface ResponseType {
@@ -22,6 +28,9 @@ export const buildRequest = async ({
   method,
   options,
   payload,
+  errorShouldNotifyUI = true,
+  onCatchErrorMsg = "",
+  errorDuration = 10000,
 }: RequestProps): Promise<ApiResponse> => {
   try {
     if (
@@ -41,12 +50,31 @@ export const buildRequest = async ({
       ...options,
       method,
     });
+
+    if (!response.ok) {
+      const contentType = response.headers.get("content-type");
+      const isJson = contentType?.includes("application/json");
+      const errorData = isJson ? await response.json() : await response.text();
+      const errorMessage =
+        errorData.detail || `HTTP error! Status: ${response.status}`;
+      throw new Error(errorMessage);
+    }
     const contentType = response.headers.get("content-type");
     const isJson = contentType?.includes("application/json");
     const data = isJson ? await response.json() : await response.text();
     const statusCode = response.status;
     return { res: data, status: statusCode };
   } catch (error) {
+    if (errorShouldNotifyUI) {
+      const errorMessage = onCatchErrorMsg || error.toString();
+      toast({
+        title: "Error",
+        description: errorMessage,
+        status: "error",
+        duration: errorDuration,
+        isClosable: true,
+      });
+    }
     return error;
   }
 };
