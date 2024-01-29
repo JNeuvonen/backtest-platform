eb5a60dfc6e86af303caed64187d20aeba0daad2
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-29 11:43:20 +0200

    Update view-backtests.tsx

--- client/pages/data/model/trainjob/view-backtests.tsx
+++ client/pages/data/model/trainjob/view-backtests.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useTrainJobBacktests } from "../../../../clients/queries/queries";
 import { usePathParams } from "../../../../hooks/usePathParams";
-import { SmallTable } from "../../../../components/tables/Small";
+import { RowItem, SmallTable } from "../../../../components/tables/Small";
 import { Spinner } from "@chakra-ui/react";
 import { WithLabel } from "../../../../components/form/WithLabel";
 import { BacktestObject } from "../../../../clients/queries/response-types";
+import { Link } from "react-router-dom";
 
 const BACKTEST_TABLE_COLUMNS = ["Id", "Start balance", "End balance"];
 
@@ -19,8 +20,21 @@ export const BacktestsPage = () => {
     return <Spinner />;
   }
 
-  const getBacktestsRows = (data: BacktestObject) => {
-    return [];
+  const getBacktestsRows = (data: BacktestObject[]) => {
+    const ret = [] as RowItem[];
+    data.sort((a, b) => {
+      return b.end_balance - a.end_balance;
+    });
+    data.forEach((item, i) => {
+      ret.push([
+        <Link className="link-default" to="/">
+          {i}
+        </Link>,
+        item.start_balance,
+        item.end_balance,
+      ]);
+    });
+    return ret;
   };
 
   return (
