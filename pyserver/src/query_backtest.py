e54ecefe6a8ac9d251d9896caca083ca99684913
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-29 11:27:29 +0200

    Update query_backtest.py

--- pyserver/src/query_backtest.py
+++ pyserver/src/query_backtest.py
@@ -1,5 +1,5 @@
 import json
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
 
 from log import LogExceptionContext
 from orm import Base, Session
@@ -11,6 +11,9 @@ class Backtest(Base):
     enter_and_exit_trade_criteria = Column(String)
     data = Column(String)
     model_weights_id = Column(Integer, ForeignKey("model_weights.id"))
+    train_job_id = Column(Integer, ForeignKey("train_job.id"))
+    start_balance = Column(Float)
+    end_balance = Column(Float)
 
     def serialize_data(self, backtest_data):
         self.data = json.dumps(backtest_data)
@@ -18,13 +21,29 @@ class Backtest(Base):
 
 class BacktestQuery:
     @staticmethod
-    def create_backtest_entry(enter_exit_criteria: str, data, model_weights_id: int):
+    def deserialize_data(backtest: Backtest):
+        if backtest and backtest.data:
+            backtest.data = json.loads(backtest.data)
+        return backtest
+
+    @staticmethod
+    def create_backtest_entry(
+        enter_exit_criteria: str,
+        data,
+        model_weights_id: int,
+        train_job_id: int,
+        start_balance: float,
+        end_balance: float,
+    ):
         with LogExceptionContext():
             with Session() as session:
                 new_backtest = Backtest(
                     enter_and_exit_trade_criteria=enter_exit_criteria,
                     data=json.dumps(data),
                     model_weights_id=model_weights_id,
+                    train_job_id=train_job_id,
+                    start_balance=start_balance,
+                    end_balance=end_balance,
                 )
                 session.add(new_backtest)
                 session.commit()
@@ -37,7 +56,7 @@ class BacktestQuery:
                 backtest_data = (
                     session.query(Backtest).filter(Backtest.id == backtest_id).first()
                 )
-                return backtest_data
+                return BacktestQuery.deserialize_data(backtest_data)
 
     @staticmethod
     def update_backtest_data(backtest_id: int, new_data: dict):
@@ -47,3 +66,14 @@ class BacktestQuery:
                     {"data": json.dumps(new_data)}
                 )
                 session.commit()
+
+    @staticmethod
+    def fetch_backtests_by_train_job_id(train_job_id: int):
+        with LogExceptionContext():
+            with Session() as session:
+                backtests = (
+                    session.query(Backtest)
+                    .filter(Backtest.train_job_id == train_job_id)
+                    .all()
+                )
+                return [BacktestQuery.deserialize_data(bt) for bt in backtests]
