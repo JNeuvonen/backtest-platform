3df5d8b603922b9a0c6be64b9c444644b9c31aac
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-29 15:55:07 +0200

    Update number.ts

--- client/utils/number.ts
+++ client/utils/number.ts
@@ -11,3 +11,34 @@ export const roundNumberDropRemaining = (
 
   return rounded;
 };
+
+export const getNumberArrayMean = (numbers: number[]) => {
+  if (numbers.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; ++i) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+};
+
+export const getArrayMin = (numbers: number[]) => {
+  let min = 999999999999;
+
+  for (let i = 0; i < numbers.length; ++i) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  return min;
+};
+
+export const getArrayMax = (numbers: number[]) => {
+  let max = -999999999999;
+
+  for (let i = 0; i < numbers.length; ++i) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+};
