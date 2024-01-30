ca969fca85da698dbf88cbbc6a7b126b1be715b9
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-30 11:45:26 +0200

    Update number.ts

--- client/utils/number.ts
+++ client/utils/number.ts
@@ -42,3 +42,64 @@ export const getArrayMax = (numbers: number[]) => {
   }
   return max;
 };
+
+export const calculateStdDevAndMean = (numbers: number[]) => {
+  const mean = getNumberArrayMean(numbers);
+  let varianceSum = 0;
+  for (let i = 0; i < numbers.length; ++i) {
+    const diffToMean = mean - numbers[i];
+    varianceSum += diffToMean * diffToMean;
+  }
+  const variance = varianceSum / numbers.length;
+  return {
+    stdDev: Math.sqrt(variance),
+    mean,
+  };
+};
+
+export const calculateZScore = (x: number, mean: number, stdDev: number) => {
+  return (x - mean) / stdDev;
+};
+
+export interface NormalDistributionItems {
+  zScoreRangeStart: number;
+  count: number;
+  label: string;
+}
+export const getNormalDistributionItems = (numbers: number[]) => {
+  const ret = [] as NormalDistributionItems[];
+
+  const { mean, stdDev } = calculateStdDevAndMean(numbers);
+  const copy = [...numbers];
+  copy.sort((a, b) => a - b);
+  let jStart = 0;
+  for (let i = -3; i < 3; i += 0.06) {
+    let itemsInZCategory = 0;
+    let categorySum = 0;
+
+    for (let j = jStart; j < copy.length; ++j) {
+      const zScore = calculateZScore(copy[j], mean, stdDev);
+      if (zScore > i && zScore <= i + 0.06) {
+        jStart = j;
+        itemsInZCategory += 1;
+        categorySum += copy[j];
+      }
+
+      if (zScore > i + 0.06) {
+        jStart = j;
+
+        break;
+      }
+    }
+
+    ret.push({
+      zScoreRangeStart: i,
+      count: itemsInZCategory,
+      label:
+        itemsInZCategory !== 0
+          ? String(roundNumberDropRemaining(categorySum / itemsInZCategory, 2))
+          : "",
+    });
+  }
+  return ret;
+};
