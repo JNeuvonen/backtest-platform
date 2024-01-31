13e14b566b5faa18659285567adadc9459b81c27
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-31 16:03:13 +0200

    Update CenteredSpinner.tsx

--- client/components/CenteredSpinner.tsx
+++ client/components/CenteredSpinner.tsx
@@ -1,7 +1,11 @@
-import { Spinner } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import React from "react";
 
-export const CenteredSpinner = () => {
+interface Props {
+  text: string;
+}
+
+export const CenteredSpinner = ({ text }: Props) => {
   return (
     <div
       style={{
@@ -9,10 +13,23 @@ export const CenteredSpinner = () => {
         alignItems: "center",
         justifyContent: "center",
         width: "100%",
-        height: "100%",
+        height: "50vh",
       }}
     >
-      <Spinner />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "50vh",
+          gap: "32px",
+        }}
+      >
+        <Heading>{text}</Heading>
+        <Spinner />
+      </div>
     </div>
   );
 };
