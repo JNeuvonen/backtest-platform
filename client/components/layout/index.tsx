ded446ce6fd0cf3a5537239568006ca29426f71b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-01-31 15:31:01 +0200

    Update index.tsx

--- client/components/layout/index.tsx
+++ client/components/layout/index.tsx
@@ -3,19 +3,29 @@ import { SideNav } from "./SideNav";
 import { useAppContext } from "../../context/App";
 import { LAYOUT } from "../../utils/constants";
 import { LayoutToolbar } from "../Toolbar";
+import { TauriTitleBar } from "../TauriTitleBar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const { toolbarMode } = useAppContext();
+  const { titleBarHeight } = useAppContext();
   return (
-    <div className="layout">
+    <div
+      className="layout"
+      style={{
+        borderRadius: "15px !important",
+      }}
+    >
+      <TauriTitleBar />
       <SideNav />
       <LayoutToolbar />
       <div
         className="layout__content"
         id="layout__content"
         style={{
-          paddingTop:
-            toolbarMode === "TRAINING" ? LAYOUT.training_toolbar_height : 0,
+          marginTop:
+            toolbarMode === "TRAINING"
+              ? LAYOUT.training_toolbar_height + titleBarHeight
+              : titleBarHeight,
         }}
       >
         {children}
