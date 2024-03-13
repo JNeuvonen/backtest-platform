e3990689db1507468184b18ef8b0312694b0f38d
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-13 20:01:34 +0200

    [FE]: Continue backtest dataset page

--- client/pages/simulate/dataset/index.tsx
+++ client/pages/simulate/dataset/index.tsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { usePathParams } from "../../../hooks/usePathParams";
+import {
+  Badge,
+  Heading,
+  MenuButton,
+  MenuItem,
+  Spinner,
+} from "@chakra-ui/react";
+import { useDatasetQuery } from "../../../clients/queries/queries";
+import { ChakraMenu } from "../../../components/chakra/Menu";
+import { FaFileImport } from "react-icons/fa6";
+import { useBacktestContext } from "../../../context/backtest";
+
+type PathParams = {
+  datasetName: string;
+};
 
 export const SimulateDatasetIndex = () => {
-  return <div>Test</div>;
+  const { datasetName } = usePathParams<PathParams>();
+  const { data } = useDatasetQuery(datasetName);
+  const { createNewDrawer } = useBacktestContext();
+
+  if (!data) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <Heading size={"lg"}>Backtest</Heading>
+
+      <div style={{ marginTop: "16px" }}>
+        Target column: <Badge colorScheme="green">{data.target_col}</Badge>
+      </div>
+      <ChakraMenu menuButton={<MenuButton>File</MenuButton>}>
+        <MenuItem icon={<FaFileImport />} onClick={createNewDrawer.onOpen}>
+          New
+        </MenuItem>
+        <MenuItem icon={<FaFileImport />}>Update target column</MenuItem>
+      </ChakraMenu>
+    </div>
+  );
 };
