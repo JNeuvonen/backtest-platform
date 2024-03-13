7badefaccd1e1752598476fd6950ba8c05c01f4d
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-13 14:15:41 +0200

    [FE]: Organize imports

--- client/theme.tsx
+++ client/theme.tsx
@@ -1,12 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
-import {
-  COLOR_BG_PRIMARY,
-  COLOR_BG_PRIMARY_SHADE_THREE,
-  COLOR_BG_TERTIARY,
-  COLOR_BRAND_SECONDARY_HIGHLIGHT,
-  COLOR_BRAND_SECONDARY_SHADE_ONE,
-  COLOR_CONTENT_PRIMARY,
-} from "./utils/colors";
+import { COLOR_BG_PRIMARY, COLOR_CONTENT_PRIMARY } from "./utils/colors";
 import {
   cardTheme,
   menuItemTheme,
