702ed711f81b496670bd87261e71f2c0e6fd4f56
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-17 11:32:52 +0200

    [FE]: Add forward and backward controls to TitleBar

--- client/components/TauriTitleBar.tsx
+++ client/components/TauriTitleBar.tsx
@@ -5,7 +5,9 @@ import { COLOR_DARK_BG_PRIMARY } from "../utils/colors";
 import { HoverableIcon } from "./HoverableIcon";
 import { PATHS } from "../utils/constants";
 import { WindowTitlebar } from "../thirdparty/tauri-controls";
-import { Spinner } from "@chakra-ui/react";
+import { IconButton, Spinner } from "@chakra-ui/react";
+import { MdArrowForward } from "react-icons/md";
+import { BUTTON_VARIANTS } from "../theme";
 
 export const TauriTitleBar = () => {
   const { titleBarHeight } = useAppContext();
@@ -45,7 +47,7 @@ export const TauriTitleBar = () => {
           height: titleBarHeight,
           display: "flex",
           alignItems: "center",
-          justifyContent: "flex-end",
+          justifyContent: "space-between",
           width: "100%",
           paddingRight: "16px",
           gap: "16px",
@@ -53,15 +55,50 @@ export const TauriTitleBar = () => {
         }}
         data-tauri-drag-region
       >
-        {!serverLaunched && (
-          <div>
-            <Spinner size={"xs"} />
-          </div>
-        )}
-        <HoverableIcon
-          icon={(props) => <FaGear {...props} />}
-          to={PATHS.settings}
-        />
+        <div
+          style={{
+            display: "flex",
+            gap: "6px",
+            alignItems: "center",
+            marginLeft: "48px",
+          }}
+        >
+          <IconButton
+            aria-label="Icon button"
+            icon={<MdArrowForward style={{ transform: "rotate(180deg)" }} />}
+            variant={BUTTON_VARIANTS.grey}
+            onClick={() => window.history.back()}
+            height={"30px"}
+            width={"30px"}
+          />
+          <IconButton
+            aria-label="Icon button"
+            icon={<MdArrowForward />}
+            variant={BUTTON_VARIANTS.grey}
+            onClick={() => window.history.forward()}
+            height={"30px"}
+            width={"30px"}
+          />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            gap: "6px",
+            alignItems: "center",
+            marginRight: "16px",
+          }}
+        >
+          {!serverLaunched && (
+            <div>
+              <Spinner size={"xs"} />
+            </div>
+          )}
+          <HoverableIcon
+            icon={(props) => <FaGear {...props} />}
+            to={PATHS.settings}
+          />
+        </div>
       </div>
     </WindowTitlebar>
   );
