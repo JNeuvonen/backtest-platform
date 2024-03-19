149e33c683e8da37ea3715b5d07abe13545b1ac3
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-19 18:25:31 +0200

    [BE]: Add CodePreset table to the ORM

---
+++ pyserver/src/query_code_preset.py
@@ -0,0 +1,42 @@
+from typing import Dict
+from sqlalchemy import Column, Integer, String
+from log import LogExceptionContext
+from orm import Base, Session
+
+
+class CodePreset(Base):
+    __tablename__ = "code_preset"
+    id = Column(Integer, primary_key=True)
+    code = Column(String)
+    category = Column(String, unique=True)
+
+
+class CodePresetQuery:
+    @staticmethod
+    def create_entry(fields: Dict):
+        with LogExceptionContext():
+            with Session() as session:
+                entry = CodePreset(**fields)
+                session.add(entry)
+                session.commit()
+                return entry.id
+
+    @staticmethod
+    def retrieve_all(preset_id: str):
+        with LogExceptionContext():
+            with Session() as session:
+                code_presets = (
+                    session.query(CodePreset)
+                    .filter(CodePreset.category == preset_id)
+                    .all()
+                )
+                return code_presets
+
+    @staticmethod
+    def fetch_one_by_id(id: int) -> CodePreset:
+        with LogExceptionContext():
+            with Session() as session:
+                code_preset = (
+                    session.query(CodePreset).filter(CodePreset.id == id).one_or_none()
+                )
+                return code_preset
