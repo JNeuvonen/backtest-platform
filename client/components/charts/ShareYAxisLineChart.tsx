5210af6785800082108225a06487241323721518
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-26 19:43:21 +0200

    [FE]: Visualize trade's enter and exit times on a chart

--- client/components/charts/ShareYAxisLineChart.tsx
+++ client/components/charts/ShareYAxisLineChart.tsx
@@ -8,7 +8,10 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
+  Scatter,
+  ComposedChart,
 } from "recharts";
+import { COLOR_BRAND_PRIMARY, COLOR_BRAND_SECONDARY } from "../../utils/colors";
 
 interface TwoLineChartProps {
   data: { [key: string]: object }[];
@@ -18,6 +21,7 @@ interface TwoLineChartProps {
   height: number;
   containerStyles?: CSSProperties;
   showDots?: boolean;
+  displayTradeEntersAndExits?: boolean;
 }
 
 export const ShareYAxisTwoLineChart = ({
@@ -28,16 +32,34 @@ export const ShareYAxisTwoLineChart = ({
   height,
   containerStyles,
   showDots = true,
+  displayTradeEntersAndExits = false,
 }: TwoLineChartProps) => (
   <ResponsiveContainer width={"100%"} height={height} style={containerStyles}>
-    <LineChart data={data}>
+    <ComposedChart data={data}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey={xKey} />
       <YAxis />
       <Tooltip />
       <Legend />
-      <Line type="monotone" dataKey={line1Key} stroke="green" dot={showDots} />
-      <Line type="monotone" dataKey={line2Key} stroke="red" dot={showDots} />
-    </LineChart>
+      <Line
+        type="monotone"
+        dataKey={line1Key}
+        stroke={COLOR_BRAND_PRIMARY}
+        dot={showDots}
+      />
+      <Line
+        type="monotone"
+        dataKey={line2Key}
+        stroke={COLOR_BRAND_SECONDARY}
+        dot={showDots}
+      />
+
+      {displayTradeEntersAndExits && (
+        <>
+          <Scatter dataKey="enter_trade" fill="green" />
+          <Scatter dataKey="exit_trade" fill="red" />
+        </>
+      )}
+    </ComposedChart>
   </ResponsiveContainer>
 );
