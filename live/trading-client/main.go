faf2e899295b8880f1fc7fef0254ebc470d38c28
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-28 19:53:31 +0200

    Update main.go

--- live/trading-client/main.go
+++ live/trading-client/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 )
 
 func main() {
-	client := NewBinanceClient()
-	res, _ := client.GetBookDepth("BTCUSDT")
-	fmt.Println(res)
+	binance := NewBinanceClient()
+	account, err := binance.Client.NewGetAccountService().Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(account)
 }
