4faca457d73ac78269bd0b7bfd54303b5c51f494
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-28 19:26:10 +0200

    Chore: add go to LoC script

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -2,16 +2,19 @@
 
 py_file_count=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/.mypy_cache/*" -not -path "*/binaries/*" | wc -l)
 ts_file_count=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/binaries/*" | wc -l)
-total_file_count=$((py_file_count + ts_file_count))
+go_file_count=$(find . -type f -name '*.go' | wc -l) 
+total_file_count=$((py_file_count + ts_file_count + go_file_count))
 
 py_lines=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/.mypy_cache/*" -not -path "*/binaries/*" | xargs cat | wc -l)
 ts_lines=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/binaries/*" | xargs cat | wc -l)
-total_line_count=$((py_lines + ts_lines))
+go_lines=$(find . -type f -name '*.go' | xargs cat | wc -l) 
+total_line_count=$((py_lines + ts_lines + go_lines))
 
 echo "Line Counts:"
 echo "------------"
 echo "Python line count: $py_lines"
 echo "TypeScript line count: $ts_lines"
+echo "Go line count: $go_lines"
 echo "Total line count: $total_line_count"
 
 echo ""
@@ -19,6 +22,7 @@ echo "File Counts:"
 echo "------------"
 echo "Python file count: $py_file_count"
 echo "TypeScript file count: $ts_file_count"
+echo "Go file count: $go_file_count"
 echo "Total file count: $total_file_count"
 
 
