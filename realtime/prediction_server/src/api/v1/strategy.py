c5cb028a3426ce901d7d6301d0b5da2a45129fd1
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-29 14:46:39 +0200

    [pred_server]: init V1 API dir

---
+++ realtime/prediction_server/src/api/v1/strategy.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Response, status
+from context import HttpResponseContext
+
+
+router = APIRouter()
+
+
+class RoutePaths:
+    STRATEGY = "/"
+
+
+@router.get(RoutePaths.STRATEGY)
+async def route_get_root():
+    with HttpResponseContext():
+        return Response(
+            content="OK", status_code=status.HTTP_200_OK, media_type="text/plain"
+        )
+
+
+@router.post(RoutePaths.STRATEGY)
+async def route_create_strategy():
+    with HttpResponseContext():
+        return Response(
+            content="OK", status_code=status.HTTP_200_OK, media_type="text/plain"
+        )
