769f31411b6b9b4a5a1e2d47bebf2e02f863d7d6
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-29 14:46:54 +0200

    [pred_server]: create HTTP res context utility

---
+++ realtime/prediction_server/src/context.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+from fastapi import HTTPException
+from log import get_logger
+
+
+@contextmanager
+def HttpResponseContext(endpoint_call_success_msg=""):
+    try:
+        yield
+        logger = get_logger()
+        if endpoint_call_success_msg != "":
+            logger.info(
+                endpoint_call_success_msg,
+            )
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
