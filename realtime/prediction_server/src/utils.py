0c47b39125520a998c5f28ef500861f0d8b44b57
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-30 10:43:44 +0200

    [pred_server]: add utility function for gen code

--- realtime/prediction_server/src/utils.py
+++ realtime/prediction_server/src/utils.py
@@ -5,3 +5,9 @@ def run_in_thread(fn, *args, **kwargs):
     thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
     thread.start()
     return thread
+
+
+def replace_placeholders_on_code_templ(code, replacements):
+    for key, value in replacements.items():
+        code = code.replace(key, str(value))
+    return code
