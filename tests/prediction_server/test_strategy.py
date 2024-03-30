f2e08d6002e941d11c09cc90cd5ff522b36bd5f5
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-30 09:31:10 +0200

    [pred_server]: add new test marker

--- tests/prediction_server/test_strategy.py
+++ tests/prediction_server/test_strategy.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from utils import Get, Post
 from fixtures.strategy import strategy_simple_1
 
@@ -8,3 +9,9 @@ def test_setup_sanity(cleanup_db):
     Post.create_strategy(body=strategy_simple_1())
     strategies = Get.fetch_strategies()
     assert len(strategies) == 1, "test_setup_sanity: no strategy was created"
+
+
+@pytest.mark.slow
+def test_strategy_polling(cleanup_db):
+    Post.create_strategy(body=strategy_simple_1())
+    time.sleep(10)
