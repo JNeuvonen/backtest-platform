ea483d7946d86fa4f1bf77fadcd9093f82a8d01a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-30 17:30:10 +0200

    [pred_server]: add auto_whitelisted_ip env var

--- realtime/prediction_server/src/config.py
+++ realtime/prediction_server/src/config.py
@@ -1,13 +1,23 @@
 import os
+import socket
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+hostname = socket.gethostname()
+
 DATABASE_URI = os.getenv("DATABASE_URI", "")
 SERVICE_PORT = os.getenv("SERVICE_PORT", "")
+AUTO_WHITELISTED_IP = os.getenv("AUTO_WHITELISTED_IP", "")
 ENV = os.getenv("ENV", "")
 LOG_FILE = "logs"
+
+
+def get_auto_whitelisted_ip():
+    if AUTO_WHITELISTED_IP == "":
+        raise Exception("No whitelisted IP was provided")
+    return AUTO_WHITELISTED_IP
 
 
 def get_db_uri():
