a11310dd7b0ff07bcd6d942af2703b6239168fe9
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-31 10:31:02 +0300

    [pred_server]: create api_key_auth middleware

--- realtime/prediction_server/src/middleware.py
+++ realtime/prediction_server/src/middleware.py
@@ -1,8 +1,17 @@
+from fastapi.security import APIKeyHeader
 from config import is_dev
+from schema.api_key import APIKeyQuery
 from schema.whitelisted_ip import WhiteListedIPQuery
-from fastapi import Request
+from fastapi import Request, Security, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse
+
+api_key_header = APIKeyHeader(name="X-API-KEY")
+
+
+async def api_key_auth(api_key: str = Security(api_key_header)):
+    if not is_dev() and not APIKeyQuery.is_valid_key(api_key):
+        raise HTTPException(status_code=401, detail="Invalid or missing API key")
 
 
 class ValidateIPMiddleware(BaseHTTPMiddleware):
@@ -11,7 +20,7 @@ class ValidateIPMiddleware(BaseHTTPMiddleware):
             return JSONResponse(status_code=403, content={"detail": "IP not allowed"})
 
         ip = request.client.host
-        if is_dev() is False and not WhiteListedIPQuery.is_allowed_ip(ip):
+        if not is_dev() and not WhiteListedIPQuery.is_allowed_ip(ip):
             return JSONResponse(status_code=403, content={"detail": "IP not allowed"})
         response = await call_next(request)
         return response
