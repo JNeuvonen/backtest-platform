61c3cf33460d2761afafa6e4ad8c8775d2858614
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-03-31 10:28:39 +0300

    [pred_server]: schema - add apikey table

---
+++ realtime/prediction_server/src/schema/api_key.py
@@ -0,0 +1,48 @@
+from typing import Dict
+from log import LogExceptionContext
+from sqlalchemy import Column, DateTime, Integer, String, func
+from orm import Base, Session
+
+
+class APIKey(Base):
+    __tablename__ = "api_keys"
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    key = Column(String, unique=True)
+
+
+class APIKeyQuery:
+    @staticmethod
+    def create_entry(fields: Dict):
+        with LogExceptionContext():
+            try:
+                with Session() as session:
+                    entry = APIKey(**fields)
+                    session.add(entry)
+                    session.commit()
+                    return entry.id
+            except Exception:
+                return "Unique constraint failed"
+
+    @staticmethod
+    def get_api_keys():
+        with LogExceptionContext():
+            with Session() as session:
+                return session.query(APIKey).all()
+
+    @staticmethod
+    def remove_api_key_by_key(key: str):
+        with LogExceptionContext():
+            with Session() as session:
+                session.query(APIKey).filter(APIKey.key == key).delete()
+                session.commit()
+
+    @staticmethod
+    def is_valid_key(key: str) -> bool:
+        with LogExceptionContext():
+            with Session() as session:
+                return (
+                    session.query(APIKey).filter(APIKey.key == key).first() is not None
+                )
