b7c684a1596afead10c1e2a4055d2948a23e911c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-02 18:22:56 +0300

    [trading_client]: create util for creating high info log exceptions

---
+++ realtime/trading_client/src/util.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func FormatCloudLog(err error, message string, functionName string) string {
+	stack := debug.Stack()
+	logMessage := fmt.Sprintf(
+		"Error in %s: %s - %s. Stack trace: %s",
+		functionName,
+		message,
+		err.Error(),
+		stack,
+	)
+	return logMessage
+}
+
+type FmtError struct {
+	error
+	stack string
+}
+
+func CaptureStack() string {
+	return string(debug.Stack())
+}
+
+func NewFmtError(err error, stack string) *FmtError {
+	if err == nil {
+		return nil
+	}
+	return &FmtError{
+		error: err,
+		stack: stack,
+	}
+}
+
+func (e *FmtError) Error() string {
+	return fmt.Sprintf("%v\nStack Trace:\n%s", e.error, e.stack)
+}
