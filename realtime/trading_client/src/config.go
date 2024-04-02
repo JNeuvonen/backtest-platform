5768a56bf700f1993a91b13f7a248333d39a9969
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-02 10:50:12 +0300

    [trading_client]: retrieve prediction service config from ENV

--- realtime/trading_client/src/config.go
+++ realtime/trading_client/src/config.go
@@ -15,6 +15,32 @@ type TradingConfig struct {
 	LiveTradingEnabled int32
 }
 
+type PredServerConfig struct {
+	URI     string
+	API_KEY string
+}
+
+func getPredServerConfig() PredServerConfig {
+	err := godotenv.Load()
+	if err != nil {
+		panic("No ENV variables provided")
+	}
+
+	predServerURI := os.Getenv("PREDICTION_SERVICE_URI")
+	predServerAPIKey := os.Getenv("PREDICTION_SERVICE_API_KEY")
+
+	if predServerURI == "" || predServerAPIKey == "" {
+		panic("PREDICTION_SERVICE_URI or PREDICTION_SERVICE_API_KEY is not set or empty")
+	}
+
+	config := PredServerConfig{
+		URI:     predServerURI,
+		API_KEY: predServerAPIKey,
+	}
+
+	return config
+}
+
 func getTradingConfig() TradingConfig {
 	err := godotenv.Load()
 	if err != nil {
