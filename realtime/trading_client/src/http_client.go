93d8aff371c9aa3ef6e6c64ef6bf1537b6300400
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-02 13:54:14 +0300

    [trading_client]: Create FetchStrategies function

--- realtime/trading_client/src/http_client.go
+++ realtime/trading_client/src/http_client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 )
@@ -50,4 +51,29 @@ func (client *HttpClient) Post(endpoint string, contentType string, data []byte)
 	defer resp.Body.Close()
 
 	return io.ReadAll(resp.Body)
+}
+
+func FetchStrategies() ([]Strategy, error) {
+	predServConfig := GetPredServerConfig()
+	headers := map[string]string{
+		"X-API-KEY": predServConfig.API_KEY,
+	}
+	client := NewHttpClient(predServConfig.URI, headers)
+
+	response, err := client.Get(PRED_SERV_V1_STRAT)
+	if err != nil {
+		return nil, err
+	}
+
+	if response != nil && len(response) > 0 {
+		var strategyResponse StrategyResponse
+		err := json.Unmarshal(response, &strategyResponse)
+		if err != nil {
+			return nil, err
+		}
+
+		return strategyResponse.Data, nil
+	} else {
+		return []Strategy{}, nil
+	}
 }
