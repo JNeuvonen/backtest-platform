bbcdfdeba15efd984d1fed7a7b5d45c2a693fe24
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-03 09:47:49 +0300

    [trading_client]: create funcs for checking exit conditions

--- realtime/trading_client/src/trading.go
+++ realtime/trading_client/src/trading.go
@@ -4,6 +4,43 @@ import (
 	"fmt"
 )
 
+func shouldStopLossClose(strat Strategy, price float64) bool {
+	if strat.IsShortSellingStrategy {
+		threshold := 1 + (strat.StopLossThresholdPerc / 100)
+		return price > strat.PriceOnTradeOpen*threshold
+	} else {
+		threshold := 1 - (strat.StopLossThresholdPerc / 100)
+		return price < strat.PriceOnTradeOpen*threshold
+	}
+}
+
+func shouldTimebasedClose(strat Strategy) bool {
+	if strat.UseTimeBasedClose {
+		return strat.KlinesLeftTillAutoclose == 0
+	}
+	return false
+}
+
+func shouldProfitBasedClose(strat Strategy, price float64) bool {
+	profitThreshold := 1 - (strat.TakeProfitThresholdPerc / 100)
+
+	if strat.IsShortSellingStrategy {
+		return price < strat.PriceOnTradeOpen*profitThreshold
+	} else {
+		return price > strat.PriceOnTradeOpen*(1+strat.TakeProfitThresholdPerc/100)
+	}
+}
+
+func CheckForStrategyClose(bc *BinanceClient, strat Strategy) bool {
+	price, err := bc.FetchLatestPrice(strat.Symbol)
+	if err != nil {
+		return false
+	}
+
+	return shouldStopLossClose(strat, price) || shouldTimebasedClose(strat) ||
+		shouldProfitBasedClose(strat, price)
+}
+
 func TradingLoop() {
 	predServConfig := GetPredServerConfig()
 	tradingConfig := GetTradingConfig()
@@ -25,17 +62,16 @@ func TradingLoop() {
 
 		strategies := predServClient.FetchStrategies()
 
-		optimal_sizes_map := make(map[string]float64)
+		optimalSizesMap := make(map[string]float64)
 
 		for _, strat := range strategies {
-			// fmt.Println(strat.Symbol)
-
-			if strat.ShouldEnterTrade {
-				optimal_sizes_map[strat.Symbol] = 0.0
+			if strat.IsInPosition {
+				_ = CheckForStrategyClose(binanceClient, strat)
+			} else if !strat.IsInPosition {
 			}
 		}
 
-		fmt.Println(optimal_sizes_map)
+		fmt.Println(optimalSizesMap)
 		predServClient.CreateCloudLog("Trading loop completed", "info")
 	}
 }
