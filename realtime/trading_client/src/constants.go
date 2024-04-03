dae3e584879c7cf8aa09ba3c91231760e080d353
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-03 16:03:44 +0300

    Update constants.go

--- realtime/trading_client/src/constants.go
+++ realtime/trading_client/src/constants.go
@@ -2,10 +2,14 @@ package main
 
 const (
 	TESTNET = "https://testnet.binance.vision"
-	MAINNET = "https://api.binance.com/api"
+	MAINNET = "https://api.binance.com"
 )
 
 const (
 	PRED_SERV_V1_STRAT = "v1/strategy"
 	PRED_SERV_V1_LOG   = "v1/log"
+)
+
+const (
+	V3_PRICE = "/api/v3/ticker/price"
 )
