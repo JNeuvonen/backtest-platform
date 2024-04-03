33cea26d247a828b10c8a7036f869493b62c1c79
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-03 08:40:05 +0300

    [trading_client]: continue trading loop development

--- realtime/trading_client/src/trading.go
+++ realtime/trading_client/src/trading.go
@@ -1,15 +1,41 @@
 package main
 
+import (
+	"fmt"
+)
+
 func TradingLoop() {
 	predServConfig := GetPredServerConfig()
+	tradingConfig := GetTradingConfig()
 	headers := map[string]string{
 		"X-API-KEY": predServConfig.API_KEY,
 	}
 	predServClient := NewHttpClient(predServConfig.URI, headers)
+	binanceClient := NewBinanceClient(tradingConfig)
+
+	accountValueInUSDT, _ := binanceClient.GetCurrentAccountWorthInUSDT()
+	fmt.Println(accountValueInUSDT)
 
 	for {
-		_ = predServClient.FetchStrategies()
+		balances := binanceClient.FetchBalances()
 
+		for _, balance := range balances.Balances {
+			fmt.Println(balance.Free)
+		}
+
+		strategies := predServClient.FetchStrategies()
+
+		optimal_sizes_map := make(map[string]float64)
+
+		for _, strat := range strategies {
+			// fmt.Println(strat.Symbol)
+
+			if strat.ShouldEnterTrade {
+				optimal_sizes_map[strat.Symbol] = 0.0
+			}
+		}
+
+		fmt.Println(optimal_sizes_map)
 		predServClient.CreateCloudLog("Trading loop completed", "info")
 	}
 }
