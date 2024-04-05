86bb29108ab231c32c3aa5841eeb021978ae7732
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-05 10:05:46 +0300

    chore: update scripts and fixtures to match new strat schema

--- scripts/pred_server_post_strategy.py
+++ scripts/pred_server_post_strategy.py
@@ -6,17 +6,23 @@ PATH_TO_PRED_SERVER = "realtime/prediction_server/src"
 sys.path.append(PATH_TO_TESTS_MAIN_DIR)
 sys.path.append(PATH_TO_PRED_SERVER)
 
-from fixtures.strategy import strategy_simple_1, create_short_strategy_simple_1
+from fixtures.strategy import (
+    strategy_simple_1,
+    create_short_strategy_simple_1,
+    create_short_strategy_simple_2,
+)
 from orm import Session
 from schema.strategy import StrategyQuery
 
 simple_strat = strategy_simple_1()
 simple_short_strat = create_short_strategy_simple_1()
+simple_short_strat_2 = create_short_strategy_simple_2()
 
 
 strategies = [
-    StrategyQuery.create_entry(simple_strat),
-    StrategyQuery.create_entry(simple_short_strat),
+    # StrategyQuery.create_entry(simple_strat),
+    # StrategyQuery.create_entry(simple_short_strat),
+    StrategyQuery.create_entry(simple_short_strat_2),
 ]
 
 
