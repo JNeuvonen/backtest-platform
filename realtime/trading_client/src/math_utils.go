3983e8524ab371914f7a937222d53ba7426e3a95
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-06 21:28:27 +0300

    [trading_client]: create math_utils.go

---
+++ realtime/trading_client/src/math_utils.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"math"
+)
+
+func RoundToPrecision(value float64, precision int32) float64 {
+	factor := math.Pow(10, float64(precision))
+	return math.Round(value*factor) / factor
+}
+
+func SafeDivide(numerator, denominator float64) float64 {
+	if denominator == 0.0 {
+		return 0.0
+	}
+	return numerator / denominator
+}
