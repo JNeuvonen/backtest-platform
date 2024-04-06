267cdb82838d6ef38201f42d8b2254d23ab05d56
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-06 18:44:39 +0300

    [trading_client]: add various string conversion util funcs

--- realtime/trading_client/src/utils.go
+++ realtime/trading_client/src/utils.go
@@ -54,6 +54,30 @@ func ParseToFloat64(s string, fallback float64) float64 {
 	return value
 }
 
+func ParseToInt(s string, bitSize int, fallback int64) int64 {
+	value, err := strconv.ParseInt(s, 10, bitSize)
+	if err != nil {
+		return fallback
+	}
+	return value
+}
+
+func ParseToInt8(s string, fallback int8) int8 {
+	return int8(ParseToInt(s, 8, int64(fallback)))
+}
+
+func ParseToInt16(s string, fallback int16) int16 {
+	return int16(ParseToInt(s, 16, int64(fallback)))
+}
+
+func ParseToInt32(s string, fallback int32) int32 {
+	return int32(ParseToInt(s, 32, int64(fallback)))
+}
+
+func ParseToInt64(s string, fallback int64) int64 {
+	return ParseToInt(s, 64, fallback)
+}
+
 func SafeDivide(numerator, denominator float64) float64 {
 	if denominator == 0.0 {
 		return 0.0
