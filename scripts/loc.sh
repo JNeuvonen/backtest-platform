8e1caaa7a0697cbe80d1d4e254c140d836da6d85
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-06 11:33:23 +0300

    chore: remove examples, and other unnecessary files from cloned thirdparty package

--- scripts/loc.sh
+++ scripts/loc.sh
@@ -2,12 +2,12 @@
 
 py_file_count=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/venv_pred_server/*' -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/.mypy_cache/*" -not -path "*/binaries/*" | wc -l)
 ts_file_count=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/binaries/*" | wc -l)
-go_file_count=$(find . -type f -name '*.go' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/.mypy_cache/*' -not -path '*/binaries/*' -not -path '*/build/*' | wc -l) 
+go_file_count=$(find . -type f -name '*.go' -not -path '*/venv/*' -not -path '*/thirdparty/*' -not -path '*/node_modules/*' -not -path '*/.mypy_cache/*' -not -path '*/binaries/*' -not -path '*/build/*' | wc -l) 
 total_file_count=$((py_file_count + ts_file_count + go_file_count))
 
 py_lines=$(find . -type f -name '*.py' -not -path '*/venv/*' -not -path '*/venv_pred_server/*' -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/.mypy_cache/*" -not -path "*/binaries/*" | xargs cat | wc -l)
 ts_lines=$(find . -type f \( -name '*.ts' -o -name '*.tsx' \) -not -path '*/node_modules/*' -not -path '*/build/*' -not -path "*/binaries/*" | xargs cat | wc -l)
-go_lines=$(find . -type f -name '*.go' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/.mypy_cache/*' -not -path '*/binaries/*' -not -path '*/build/*' | xargs cat | wc -l) 
+go_lines=$(find . -type f -name '*.go' -not -path '*/venv/*' -not -path '*/node_modules/*' -not -path '*/thirdparty/*' -not -path '*/.mypy_cache/*' -not -path '*/binaries/*' -not -path '*/build/*' | xargs cat | wc -l) 
 total_line_count=$((py_lines + ts_lines + go_lines))
 
 echo "Line Counts:"
