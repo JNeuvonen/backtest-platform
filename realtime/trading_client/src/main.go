979bfde43097b18fd9d8cda618c0bbe59fa9bb58
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-06 21:32:04 +0300

    Update main.go

--- realtime/trading_client/src/main.go
+++ realtime/trading_client/src/main.go
@@ -1,5 +1,33 @@
 package main
 
 func main() {
-	TradingLoop()
+	predServConfig := GetPredServerConfig()
+	tradingConfig := GetTradingConfig()
+	accountName := GetAccountName()
+
+	headers := map[string]string{
+		"X-API-KEY": predServConfig.API_KEY,
+	}
+	predServClient := NewHttpClient(predServConfig.URI, headers)
+	binanceClient := NewBinanceClient(tradingConfig)
+
+	for {
+		strategies := predServClient.FetchStrategies()
+		account, _ := predServClient.FetchAccount(accountName)
+
+		for _, strat := range strategies {
+
+			if account.PreventAllTrading ||
+				GetNumFailedCallsToPredServer() >= FAILED_CALLS_TO_UPDATE_STRAT_STATE_LIMIT {
+				continue
+			}
+
+			if strat.IsInPosition && ShouldCloseTrade(binanceClient, strat) {
+				CloseStrategyTrade(binanceClient, strat)
+			} else if !strat.IsInPosition && ShouldEnterTrade(strat) {
+				EnterStrategyTrade(binanceClient, strat, account)
+			}
+		}
+		predServClient.CreateCloudLog("Trading loop completed", "info")
+	}
 }
