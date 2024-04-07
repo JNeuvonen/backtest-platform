d51c58eea6302ed2ea76401625567ad745adec4d
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-07 19:20:33 +0300

    [trading_client]: remove unused code

--- realtime/trading_client/src/log.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func LogAndRetFallback[T any](err error, defaultValue T) T {
-	CreateCloudLog(
-		NewFmtError(err, CaptureStack()).Error(),
-		LOG_EXCEPTION,
-	)
-	return defaultValue
-}
