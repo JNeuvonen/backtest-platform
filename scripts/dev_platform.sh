cd0119f459fe3aa0c221ddf2c09568786e664b6e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-15 12:47:11 +0300

    chore: update scripts

--- scripts/dev_platform.sh
+++ scripts/dev_platform.sh
@@ -1,3 +1,4 @@
 cd backtest_platform
 export REACT_APP_PRED_SERVER_URI="http://34.146.92.194"
+# export REACT_APP_PRED_SERVER_URI="http://localhost:8001" 
 npx kill-port 8000 && npm run tauri dev
--- scripts/dev_prediction_server.sh
+++ scripts/dev_prediction_server.sh
@@ -1,6 +1,9 @@
-export DATABASE_URI=postgresql://username:password@localhost/live_env_local_test
-export SERVICE_PORT=8000
+export DATABASE_URI=postgresql://postgres:password@localhost:5432/live_env_local_test
+# export DATABASE_URI=postgresql://postgres:HJyGHt67hRn8Ru@35.187.213.213:5432/prediction_server
+export SERVICE_PORT=8001
 export ENV=DEV
+export AUTO_WHITELISTED_IP="87.94.138.211"
+
 # npx kill-port $SERVICE_PORT && python realtime/prediction_server/src/main.py
 cd realtime
 cd prediction_server
