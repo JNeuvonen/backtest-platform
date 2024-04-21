97390ee9dbf9a119a89a5e2334f4f533e931f70e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-21 14:21:32 +0300

    [platform-FE]: add a lot of utility functions for creating aggregate statistics on diversfied strategy

--- backtest_platform/client/pages/mass-backtest/backtest/index.tsx
+++ backtest_platform/client/pages/mass-backtest/backtest/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { usePathParams } from "../../../hooks/usePathParams";
 import {
   useManyBacktests,
@@ -10,13 +10,15 @@ import {
   LINE_CHART_COLORS,
   MAX_NUMBER_OF_LINES,
   binarySearch,
-  getLineChartColors,
 } from "../../../utils/algo";
 import { ShareYAxisMultilineChart } from "../../../components/charts/ShareYAxisMultiline";
 import { Line } from "recharts";
-import { COLOR_BRAND_PRIMARY } from "../../../utils/colors";
 import { useMessageListener } from "../../../hooks/useMessageListener";
 import { DOM_EVENT_CHANNELS } from "../../../utils/constants";
+import { GenericRangeSlider } from "../../../components/chakra/RangeSlider";
+import { isFinite, round } from "lodash";
+import { ChakraSelect } from "../../../components/chakra/Select";
+import { getKeysCount } from "../../../utils/object";
 
 interface PathParams {
   massBacktestId: number;
@@ -43,8 +45,102 @@ const getMassSimFindTicks = (
   return ret;
 };
 
-const getMassSimEquityCurvesData = (bulkFetchBacktest: FetchBulkBacktests) => {
+const convertToReturn = (currItem: object, prevItem: object, key: string) => {
+  const currVal = currItem[key];
+  const prevVal = prevItem[key];
+
+  if (prevVal === 0) {
+    return 1;
+  }
+
+  const value = currVal / prevVal;
+
+  if (isNaN(value) || !isFinite(value)) {
+    return 1;
+  }
+
+  return value;
+};
+
+const getEquityCurveStatistics = (
+  bulkFetchBacktest: FetchBulkBacktests,
+  balances: object[],
+  returns: object[],
+  backtestKeyWithMostKlines: string
+) => {
+  const endBalances = {};
+
+  bulkFetchBacktest.equity_curves.forEach((item) => {
+    for (const [key, equityCurveTicks] of Object.entries(item)) {
+      if (key === "kline_open_time") {
+        continue;
+      }
+      for (let i = equityCurveTicks.length - 1; i >= 0; --i) {
+        const tick = equityCurveTicks[i];
+
+        if (tick.portfolio_worth) {
+          endBalances[bulkFetchBacktest.id_to_dataset_name_map[key]] =
+            tick.portfolio_worth;
+          break;
+        }
+      }
+    }
+  });
+
+  let multiStratCurrBalance = 10000;
+  const multiStrategyEquityCurve: object[] = [];
+
+  const totalStrats = getKeysCount(returns[0]) - 1;
+
+  for (let i = 0; i < returns.length; ++i) {
+    const returnsTick = returns[i];
+
+    const tick = {};
+    const roundReturns = {};
+
+    if (i >= returns.length - 10) {
+      console.log(returnsTick);
+    }
+
+    for (const [key, value] of Object.entries(returnsTick)) {
+      if (key === "kline_open_time") {
+        tick["kline_open_time"] = value;
+        continue;
+      }
+      const coeff = value === 0 ? 1 : value;
+
+      roundReturns[key] = (multiStratCurrBalance / totalStrats) * coeff;
+    }
+
+    let tickBalance = 0;
+
+    for (const [_, value] of Object.entries(roundReturns)) {
+      tickBalance += value as number;
+    }
+
+    multiStratCurrBalance = tickBalance;
+    tick["equity"] = multiStratCurrBalance;
+    multiStrategyEquityCurve.push(tick);
+  }
+
+  return {
+    endBalances: endBalances,
+    multiStrategyEquityCurve: multiStrategyEquityCurve,
+  };
+};
+
+const getMassSimEquityCurvesData = (
+  bulkFetchBacktest: FetchBulkBacktests,
+  dateRangeFilter: [number, number],
+  {
+    sinceYearFilter,
+    selectedYearFilter,
+  }: { sinceYearFilter: string; selectedYearFilter: string }
+) => {
+  if (!bulkFetchBacktest || !bulkFetchBacktest.data) return null;
+
   const ret: object[] = [];
+  const yearsUsedInBacktest = new Set();
 
   let backtestKeyWithMostKlines = "";
   let mostKlinesCount = 0;
@@ -61,7 +157,31 @@ const getMassSimEquityCurvesData = (bulkFetchBacktest: FetchBulkBacktests) => {
   bulkFetchBacktest.equity_curves.forEach((item) => {
     for (const [key, equityCurveTicks] of Object.entries(item)) {
       if (key === backtestKeyWithMostKlines) {
-        equityCurveTicks.forEach((balance) => {
+        const n = equityCurveTicks.length;
+        equityCurveTicks.forEach((balance, idx) => {
+          const timestampToDate = new Date(balance["kline_open_time"]);
+          yearsUsedInBacktest.add(timestampToDate.getFullYear());
+          if (
+            idx / n < dateRangeFilter[0] / 100 ||
+            idx / n > dateRangeFilter[1] / 100
+          ) {
+            return;
+          }
+
+          if (
+            selectedYearFilter !== "all" &&
+            String(timestampToDate.getFullYear()) !== selectedYearFilter
+          ) {
+            return;
+          }
+
+          if (
+            sinceYearFilter !== "none" &&
+            timestampToDate.getFullYear() < Number(sinceYearFilter)
+          ) {
+            return;
+          }
+
           const portfolioValues = getMassSimFindTicks(
             bulkFetchBacktest,
             balance.kline_open_time
@@ -76,7 +196,72 @@ const getMassSimEquityCurvesData = (bulkFetchBacktest: FetchBulkBacktests) => {
     }
   });
 
-  return ret;
+  const returns = [] as object[];
+
+  for (let i = 1; i < ret.length; ++i) {
+    const prevItem = ret[i - 1];
+    const currItem = ret[i];
+
+    const tick = {
+      kline_open_time: currItem["kline_open_time"],
+    };
+
+    for (const [key, _] of Object.entries(currItem)) {
+      if (key === "kline_open_time") {
+        continue;
+      }
+      const value = convertToReturn(currItem, prevItem, key);
+      tick[key] = value;
+    }
+    returns.push(tick);
+  }
+
+  const final = [] as object[];
+  const helper = {};
+
+  for (let i = 0; i < returns.length; ++i) {
+    const currItem = returns[i];
+    if (i === 0) {
+      for (const [key] of Object.entries(currItem)) {
+        if (key === "kline_open_time") {
+          continue;
+        }
+        helper[key] = 1;
+      }
+      continue;
+    }
+
+    const tick = {
+      kline_open_time: currItem["kline_open_time"],
+    };
+
+    for (const [key, value] of Object.entries(currItem)) {
+      if (key === "kline_open_time") {
+        continue;
+      }
+      if (value === undefined || value === null || isNaN(value) || value == 0) {
+        tick[key] = (helper[key] - 1) * 100;
+        continue;
+      }
+      helper[key] = helper[key] * value;
+      tick[key] = (helper[key] - 1) * 100;
+    }
+
+    final.push(tick);
+  }
+
+  const eqCurveStatistics = getEquityCurveStatistics(
+    bulkFetchBacktest,
+    ret,
+    returns,
+    backtestKeyWithMostKlines
+  );
+
+  return {
+    equityCurves: final,
+    years: Array.from(yearsUsedInBacktest),
+    ...eqCurveStatistics,
+  };
 };
 
 const getLineKeys = (bulkFetchBacktest: FetchBulkBacktests) => {
@@ -97,6 +282,9 @@ export const InvidualMassbacktestDetailsPage = () => {
     massBacktestQuery.data?.backtest_ids || [],
     true
   );
+  const [dateRangeFilter, setDateRangeFilter] = useState([0, 100]);
+  const [selectedYearFilter, setSelectedYearFilter] = useState("all");
+  const [sinceYearFilter, setSinceYearFilter] = useState("none");
 
   useMessageListener({
     messageName: DOM_EVENT_CHANNELS.refetch_component,
@@ -105,6 +293,19 @@ export const InvidualMassbacktestDetailsPage = () => {
       useManyBacktestsQuery.refetch();
     },
   });
+
+  const equityCurves = useMemo(() => {
+    return getMassSimEquityCurvesData(
+      useManyBacktestsQuery.data as FetchBulkBacktests,
+      dateRangeFilter as [number, number],
+      { selectedYearFilter, sinceYearFilter }
+    );
+  }, [
+    useManyBacktestsQuery.data,
+    dateRangeFilter,
+    selectedYearFilter,
+    sinceYearFilter,
+  ]);
 
   if (
     massBacktestQuery.isLoading ||
@@ -115,34 +316,110 @@ export const InvidualMassbacktestDetailsPage = () => {
     return <Spinner />;
   }
 
-  const equityCurves = getMassSimEquityCurvesData(useManyBacktestsQuery.data);
   const datasetSymbols = getLineKeys(useManyBacktestsQuery.data);
 
   return (
     <div>
-      <ShareYAxisMultilineChart
-        height={500}
-        data={equityCurves}
-        xAxisKey={"kline_open_time"}
-        xAxisTickFormatter={(tick: number) =>
-          new Date(tick).toLocaleDateString()
-        }
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        {datasetSymbols.map((item, idx) => {
-          return (
-            <Line
-              type="monotone"
-              dataKey={item}
-              stroke={
-                idx > MAX_NUMBER_OF_LINES
-                  ? LINE_CHART_COLORS[idx % MAX_NUMBER_OF_LINES]
-                  : LINE_CHART_COLORS[idx]
-              }
-              dot={false}
-            />
-          );
-        })}
-      </ShareYAxisMultilineChart>
+        {/* <GenericRangeSlider
+    minValue={0}
+    maxValue={100}
+    onChange={setDateRangeFilter}
+    values={dateRangeFilter}
+    label={"Date range %"}
+    containerStyle={{ width: "400px" }}
+  /> */}
+
+        <div></div>
+
+        <div style={{ gap: "16px", display: "flex", alignItems: "center" }}>
+          <ChakraSelect
+            label={"Since year"}
+            containerStyle={{ width: "150px" }}
+            options={[
+              { label: "None", value: "none" },
+              ...equityCurves["years"].map((item) => ({
+                label: String(item),
+                value: String(item),
+              })),
+            ]}
+            onChange={(value: string) => {
+              setSelectedYearFilter("all");
+              setSinceYearFilter(value);
+            }}
+          />
+          <ChakraSelect
+            label={"Filter by year"}
+            containerStyle={{ width: "150px" }}
+            options={[
+              { label: "All", value: "all" },
+              ...equityCurves["years"].map((item) => ({
+                label: String(item),
+                value: String(item),
+              })),
+            ]}
+            onChange={(value: string) => {
+              setSinceYearFilter("none");
+              setSelectedYearFilter(value);
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <ShareYAxisMultilineChart
+          height={500}
+          data={equityCurves === null ? [] : equityCurves["equityCurves"]}
+          xAxisKey={"kline_open_time"}
+          xAxisTickFormatter={(tick: number) =>
+            new Date(tick).toLocaleDateString("default", {
+              year: "numeric",
+              month: "short",
+            })
+          }
+          yAxisTickFormatter={(value: number) => `${value}%`}
+        >
+          {datasetSymbols.map((item, idx) => {
+            return (
+              <Line
+                type="monotone"
+                dataKey={item}
+                stroke={
+                  idx > MAX_NUMBER_OF_LINES
+                    ? LINE_CHART_COLORS[idx % MAX_NUMBER_OF_LINES]
+                    : LINE_CHART_COLORS[idx]
+                }
+                dot={false}
+                key={item}
+              />
+            );
+          })}
+        </ShareYAxisMultilineChart>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <ShareYAxisMultilineChart
+          height={500}
+          data={
+            equityCurves === null
+              ? []
+              : equityCurves["multiStrategyEquityCurve"]
+          }
+          xAxisKey={"kline_open_time"}
+          xAxisTickFormatter={(tick: number) =>
+            new Date(tick).toLocaleDateString("default", {
+              year: "numeric",
+              month: "short",
+            })
+          }
+        >
+          <Line type="monotone" dataKey={"equity"} stroke={"red"} dot={false} />
+        </ShareYAxisMultilineChart>
+      </div>
     </div>
   );
 };
