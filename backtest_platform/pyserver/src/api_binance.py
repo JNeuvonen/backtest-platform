c01ede441021abcb9247b12ccbb684ed3a11dfc6
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-04-30 16:55:57 +0300

    [platform-BE]: Add non_async version of get_historical_klines

--- backtest_platform/pyserver/src/api_binance.py
+++ backtest_platform/pyserver/src/api_binance.py
@@ -40,11 +40,66 @@ async def get_historical_klines(symbol, interval):
     return df
 
 
+def non_async_get_historical_klines(symbol, interval):
+    client = Client()
+    start_time = "1 Jan, 2017"
+    klines = []
+
+    while True:
+        new_klines = client.get_historical_klines(
+            symbol=symbol, interval=interval, start_str=start_time, limit=1000
+        )
+        if not new_klines:
+            break
+
+        klines += new_klines
+        start_time = int(new_klines[-1][0]) + 1
+
+    df = pd.DataFrame(klines, columns=BINANCE_DATA_COLS)
+    df.drop(["ignore", "kline_close_time"], axis=1, inplace=True)
+    df["kline_open_time"] = pd.to_numeric(df["kline_open_time"])
+
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    df.sort_values("kline_open_time", inplace=True)
+    return df
+
+
 async def save_historical_klines(symbol, interval, send_msg_to_fe=True):
     with LogExceptionContext(notification_duration=60000):
         logger = get_logger()
         datasets_conn = create_connection(AppConstants.DB_DATASETS)
         klines = await get_historical_klines(symbol, interval)
+        table_name = get_binance_dataset_tablename(symbol, interval)
+
+        table_exists_query = (
+            f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+        )
+        cursor = datasets_conn.cursor()
+        cursor.execute(table_exists_query)
+        if cursor.fetchone():
+            logger.log(f"Table {table_name} already exists.", logging.INFO)
+            DatasetQuery.delete_entry_by_dataset_name(table_name)
+
+        klines.to_sql(table_name, datasets_conn, if_exists="replace", index=False)
+        DatasetQuery.create_dataset_entry(table_name, "kline_open_time")
+
+        if send_msg_to_fe is True:
+            logger.log(
+                f"Downloaded klines on {symbol} with {interval} interval",
+                logging.INFO,
+                True,
+                True,
+                DomEventChannels.REFETCH_ALL_DATASETS.value,
+            )
+
+
+def non_async_save_historical_klines(symbol, interval, send_msg_to_fe=True):
+    with LogExceptionContext(notification_duration=60000):
+        logger = get_logger()
+        datasets_conn = create_connection(AppConstants.DB_DATASETS)
+        klines = non_async_get_historical_klines(symbol, interval)
         table_name = get_binance_dataset_tablename(symbol, interval)
 
         table_exists_query = (
