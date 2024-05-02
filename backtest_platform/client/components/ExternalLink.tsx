bba4730d5ac799186382aca3543de4a11222e27a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-02 23:01:10 +0300

    [platform-FE]: External link can now open link in users default browser

--- backtest_platform/client/components/ExternalLink.tsx
+++ backtest_platform/client/components/ExternalLink.tsx
@@ -2,16 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { COLOR_LINK_DEFAULT } from "../utils/colors";
+import { open } from "@tauri-apps/api/shell";
 
 const ExternalLink = ({
   to,
   linkText,
   iconSize = "13px",
   iconColor = COLOR_LINK_DEFAULT,
+  isExternal = false,
 }) => {
+  const handleOpenLink = async (url: string) => {
+    await open(url);
+  };
   return (
     <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-      <Link to={to} className="link-default">
+      <Link
+        to={to}
+        className="link-default"
+        onClick={(e) => {
+          if (isExternal) {
+            e.preventDefault();
+            handleOpenLink(to);
+          }
+        }}
+      >
         {linkText}
       </Link>
       <div>
