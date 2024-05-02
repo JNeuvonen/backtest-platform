cc8dea984e2a76f8e65722a8e1540506fd77d705
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-02 18:00:11 +0300

    [platform-FE]: ChakraCard add styles

--- backtest_platform/client/components/chakra/Card.tsx
+++ backtest_platform/client/components/chakra/Card.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Card, CardBody, CardHeader } from "@chakra-ui/react";
 
 interface Props {
   children: React.ReactNode;
   heading?: React.ReactNode;
   variant?: string;
+  containerStyles?: CSSProperties;
 }
-export const ChakraCard = ({ children, heading, variant }: Props) => {
+export const ChakraCard = ({
+  children,
+  heading,
+  variant,
+  containerStyles,
+}: Props) => {
   return (
-    <Card variant={variant}>
+    <Card variant={variant} style={containerStyles}>
       {heading && <CardHeader>{heading}</CardHeader>}
       <CardBody>{children}</CardBody>
     </Card>
