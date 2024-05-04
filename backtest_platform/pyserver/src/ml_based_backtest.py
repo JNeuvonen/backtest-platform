dfb3f5295933155d27024aa231c37668ddd233ec
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-04 13:41:31 +0300

    [platform-BE]: Continue dev of ml_based_backtest v2

--- backtest_platform/pyserver/src/ml_based_backtest.py
+++ backtest_platform/pyserver/src/ml_based_backtest.py
@@ -1,9 +1,27 @@
-from backtest_utils import MLBasedBacktest
+from api_binance import non_async_save_historical_klines, save_historical_klines
+from backtest_utils import MLBasedBacktest, run_transformations_on_dataset
+from constants import BINANCE_BACKTEST_PRICE_COL
+from dataset import read_dataset_to_mem
+from query_data_transformation import DataTransformationQuery
 from query_dataset import DatasetQuery
 from request_types import BodyMLBasedBacktest
 
 
 async def run_ml_based_backtest(body: BodyMLBasedBacktest):
     dataset = DatasetQuery.fetch_dataset_by_name(body.dataset_name)
+    data_transformations = DataTransformationQuery.get_transformations_by_dataset(
+        dataset.id
+    )
+
+    if body.fetch_latest_data is True:
+        non_async_save_historical_klines(dataset.symbol, dataset.interval, True)
+        DatasetQuery.update_price_column(
+            dataset.dataset_name, BINANCE_BACKTEST_PRICE_COL
+        )
+        run_transformations_on_dataset(data_transformations, dataset)
+
+    dataset_df = read_dataset_to_mem(dataset.dataset_name)
+
+    columns = dataset_df.columns.tolist()
+
     backtest = MLBasedBacktest(body, dataset)
-    pass
