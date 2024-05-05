04d39224cbf3f1dd8e9e0322fa9039fe6a701ac3
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-05 16:01:46 +0300

    [platform]: Change PRED_SERVER_URI in startup script

--- scripts/dev_platform.sh
+++ scripts/dev_platform.sh
@@ -1,4 +1,6 @@
 cd backtest_platform
-export REACT_APP_PRED_SERVER_URI="http://34.146.92.194"
+
+#no trailing slash!
+export REACT_APP_PRED_SERVER_URI="http://35.187.220.106"
 # export REACT_APP_PRED_SERVER_URI="http://localhost:8001" 
 npx kill-port 8000 && npm run tauri dev
