b43e76931c10e70610f1e4e071b90373f8a9a797
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-06 18:38:28 +0300

    [pred_serv]: Create calculate_timestamp_for_kline_fetch

--- realtime/prediction_server/src/utils.py
+++ realtime/prediction_server/src/utils.py
@@ -3,6 +3,9 @@ import time
 import threading
 
 
+NUM_REQ_KLINES_BUFFER = 5
+
+
 def run_in_thread(fn, *args, **kwargs):
     thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
     thread.start()
@@ -21,3 +24,11 @@ def file_exists(file_path: str) -> bool:
 
 def get_current_timestamp_ms() -> int:
     return int(time.time() * 1000)
+
+
+def calculate_timestamp_for_kline_fetch(num_required_klines, kline_size_ms):
+    curr_time_ms = get_current_timestamp_ms()
+
+    return curr_time_ms - (
+        kline_size_ms * (num_required_klines + NUM_REQ_KLINES_BUFFER)
+    )
