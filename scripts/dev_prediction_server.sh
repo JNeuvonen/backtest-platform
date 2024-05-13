99d6e6b703eb7e0168aafa757e8f354cdb7f1b4b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-13 15:57:19 +0300

    Chore: Add quatations to env variable string

--- scripts/dev_prediction_server.sh
+++ scripts/dev_prediction_server.sh
@@ -1,4 +1,4 @@
-export DATABASE_URI=postgresql://postgres:password@localhost:5432/live_env_local_test
+export DATABASE_URI="postgresql://postgres:password@localhost:5432/live_env_local_test"
 # export DATABASE_URI=postgresql://postgres:HJyGHt67hRn8Ru@35.187.213.213:5432/prediction_server
 export SERVICE_PORT=8001
 export ENV=DEV
--- scripts/test_prediction_server.sh
+++ scripts/test_prediction_server.sh
@@ -4,7 +4,7 @@ export TEST_RUN_PORT=3002
 export SERVICE_PORT=3002
 export ENV=DEV
 export IS_TESTING="1"
-export DATABASE_URI=postgresql://postgres:password@localhost/pred_server_integration_tests
+export DATABASE_URI="postgresql://postgres:password@localhost/pred_server_integration_tests"
 
 pytest "tests/prediction_server" -s -m "acceptance"
 
