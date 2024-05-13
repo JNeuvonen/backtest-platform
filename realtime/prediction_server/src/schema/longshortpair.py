c7232ee30ea2402c024b431d710985ee54d82e90
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-13 15:54:12 +0300

    [pred_serv]: Create longshortpair schema

---
+++ realtime/prediction_server/src/schema/longshortpair.py
@@ -0,0 +1,58 @@
+from typing import Dict
+from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer
+from orm import Base, Session
+from log import LogExceptionContext
+
+
+class LongShortPair(Base):
+    __tablename__ = "long_short_pair"
+
+    id = Column(Integer, primary_key=True)
+
+    long_short_group_id = Column(Integer, ForeignKey("long_short_group.id"))
+    buy_ticker_id = Column(Integer, ForeignKey("long_short_ticker.id"))
+    sell_ticker_id = Column(Integer, ForeignKey("long_short_ticker.id"))
+
+    buy_open_time = Column(Integer)
+    sell_open_time = Column(Integer)
+
+    buy_open_price = Column(Float)
+    sell_open_price = Column(Float)
+    buy_open_qty = Column(Float)
+    sell_open_qty = Column(Float)
+
+    in_position = Column(Boolean)
+
+
+class LongShortPairQuery:
+    @staticmethod
+    def create_entry(fields: Dict):
+        with LogExceptionContext():
+            with Session() as session:
+                pair = LongShortPair(**fields)
+                session.add(pair)
+                session.commit()
+                return pair.id
+
+    @staticmethod
+    def get_pairs_by_group_id(group_id: int):
+        with LogExceptionContext():
+            with Session() as session:
+                return (
+                    session.query(LongShortPair)
+                    .filter(LongShortPair.long_short_group_id == group_id)
+                    .all()
+                )
+
+    @staticmethod
+    def update_entry(pair_id, update_fields: Dict):
+        with LogExceptionContext():
+            with Session() as session:
+                update_fields.pop("id", None)
+                non_null_update_fields = {
+                    k: v for k, v in update_fields.items() if v is not None
+                }
+                session.query(LongShortPair).filter(LongShortPair.id == pair_id).update(
+                    non_null_update_fields, synchronize_session="fetch"
+                )
+                session.commit()
