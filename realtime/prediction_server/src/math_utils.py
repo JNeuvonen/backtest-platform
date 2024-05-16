7dcfc42d0ef8782f5a4896785de6e3a5ea0e8239
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-16 12:59:20 +0300

    [pred_serv]: add safe_divide math util func

--- realtime/prediction_server/src/math_utils.py
+++ realtime/prediction_server/src/math_utils.py
@@ -25,3 +25,9 @@ def calc_long_trade_perc_result(quantity, open_price, close_price) -> float:
     quote_amount_on_open = quantity * open_price
     quote_amount_on_close = quantity * close_price
     return (quote_amount_on_close / quote_amount_on_open - 1) * 100
+
+
+def safe_divide(num, denom, fallback=None):
+    if denom == 0.0 or denom == 0:
+        return fallback
+    return num / denom
