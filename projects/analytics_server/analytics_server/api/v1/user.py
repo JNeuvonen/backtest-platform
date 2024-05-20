9de6ed799661c7b08ac1f093fd6608d268705722
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-20 19:17:35 +0300

    [analytics_serv]: Create endpoint for creating user

--- projects/analytics_server/analytics_server/api/v1/user.py
+++ projects/analytics_server/analytics_server/api/v1/user.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, Depends, status, Response
 from common_python.http_utils import HttpResponse
+from common_python.pred_serv_models.user import UserQuery
+from common_python.http_utils import verify_ip_whitelisted
+from analytics_server.api.v1.request_types import BodyCreateuser
 
 
 router = APIRouter()
@@ -14,4 +17,15 @@ async def route_get_root():
     with HttpResponse():
         return Response(
             content="Curious?", media_type="text/plain", status_code=status.HTTP_200_OK
+        )
+
+
+@router.post(RoutePaths.ROOT, dependencies=[Depends(verify_ip_whitelisted)])
+async def route_create_entry(body: BodyCreateuser):
+    with HttpResponse():
+        id = UserQuery.create_entry(body.model_dump())
+        return Response(
+            content=str(id),
+            media_type="text/plain",
+            status_code=status.HTTP_200_OK,
         )
