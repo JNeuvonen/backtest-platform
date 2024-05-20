bfcf7dd41498f788484d1109008f3a42c883ba4c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-20 11:56:02 +0300

    [analytics]: Continue dev of Sidenav

--- analytics/analyticsweb/src/layout/Sidenav.tsx
+++ analytics/analyticsweb/src/layout/Sidenav.tsx
@@ -1,14 +1,45 @@
 import { PATHS } from "../utils";
 import { MdDashboardCustomize } from "react-icons/md";
+import { FaChartLine } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useActivePath } from "src/hooks";
 
 const SIDE_NAV_ITEMS = [
   {
-    link: "Dashboard",
+    link: "Live",
     icon: MdDashboardCustomize,
     path: PATHS.dashboard,
+  },
+  {
+    link: "Strategies",
+    icon: FaChartLine,
+    path: PATHS.strategies,
   },
 ];
 
 export const SideNav = () => {
-  return <nav className="side-nav">nav</nav>;
+  const { activePath } = useActivePath({ tabPathDepth: 1 });
+  return (
+    <nav className="side-nav">
+      <div className="side-nav__nav-items">
+        {SIDE_NAV_ITEMS.map((item) => {
+          const Icon = item.icon;
+          return (
+            <Link to={item.path} key={item.path}>
+              <div
+                className={
+                  item.path.includes(activePath)
+                    ? "side-nav__item side-nav__item-active"
+                    : "side-nav__item"
+                }
+              >
+                {<Icon />}
+                {item.link}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
--- analytics/analyticsweb/src/utils/app.ts
+++ analytics/analyticsweb/src/utils/app.ts
@@ -1,6 +1,8 @@
-const DASHBOARD_PATH = "/dashboard";
+const LIVE_PATH = "/live";
+const STRATEGIES_PATH = "/strategies";
 
 export const PATHS = {
   root: "/",
-  dashboard: DASHBOARD_PATH,
+  dashboard: LIVE_PATH,
+  strategies: STRATEGIES_PATH,
 };
