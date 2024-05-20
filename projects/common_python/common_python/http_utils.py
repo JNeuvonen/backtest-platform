5670919f7c4e15824a400704f3fb7d14f4c9ca6e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-20 19:17:49 +0300

    [common-py]: Http utils - add verify_ip_whitelisted func

--- projects/common_python/common_python/http_utils.py
+++ projects/common_python/common_python/http_utils.py
@@ -1,5 +1,8 @@
 from contextlib import contextmanager
-from fastapi import HTTPException
+from fastapi import HTTPException, Request
+
+from common_python.pred_serv_models.whitelisted_ip import WhiteListedIPQuery
+from common_python.server_config import is_dev
 
 
 @contextmanager
@@ -13,3 +16,14 @@ def HttpResponse(success_callback=None, fail_callback=None):
             fail_callback(e)
         else:
             raise HTTPException(status_code=400, detail=str(e))
+
+
+async def verify_ip_whitelisted(request: Request):
+    if request.client is None:
+        raise HTTPException(status_code=403, detail="IP not allowed")
+
+    ip = request.client.host
+    if not is_dev() and not WhiteListedIPQuery.is_allowed_ip(ip):
+        raise HTTPException(status_code=403, detail="IP not allowed")
+
+    return request
