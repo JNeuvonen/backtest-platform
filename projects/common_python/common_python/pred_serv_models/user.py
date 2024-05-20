08a8d6d77c21e802e99f981c751ef9ef34db2731
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-20 17:10:11 +0300

    [main-db]: Add user schema

---
+++ projects/common_python/common_python/pred_serv_models/user.py
@@ -0,0 +1,56 @@
+from typing import Dict
+from sqlalchemy import Column, DateTime, Integer, String, func
+from common_python.pred_serv_orm import Base, Session
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    access_level = Column(Integer)
+
+
+class UserQuery:
+    @staticmethod
+    def create_entry(fields: Dict):
+        with Session() as session:
+            entry = User(**fields)
+            session.add(entry)
+            session.commit()
+            return entry.id
+
+    @staticmethod
+    def get_users():
+        with Session() as session:
+            return session.query(User).all()
+
+    @staticmethod
+    def update_user(user_id: int, update_fields: Dict):
+        with Session() as session:
+            update_fields.pop("id", None)
+            non_null_update_fields = {
+                k: v for k, v in update_fields.items() if v is not None
+            }
+            session.query(User).filter(User.id == user_id).update(
+                non_null_update_fields, synchronize_session="fetch"
+            )
+            session.commit()
+
+    @staticmethod
+    def get_user_by_id(user_id: int):
+        with Session() as session:
+            return session.query(User).filter(User.id == user_id).first()
+
+    @staticmethod
+    def get_user_by_email(email: str):
+        with Session() as session:
+            return session.query(User).filter(User.email == email).first()
