f73ca05c3a879e976346a6d5dd9617a67f1da305
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-21 12:35:47 +0300

    [analytics_serv]: Test - Add test_token to analytics_serv requests

--- projects/analytics_server/tests/http_utils.py
+++ projects/analytics_server/tests/http_utils.py
@@ -3,11 +3,13 @@ from contextlib import contextmanager
 from common_python.test_utils.conf import TEST_RUN_PORT
 from analytics_server.api.v1.user import RoutePaths as UserRoutePaths
 from analytics_server.main import Routers
+from projects.analytics_server.tests.fixtures.test_token import TEST_TOKEN
 
 
 @contextmanager
 def Req(method, url, **kwargs):
     headers = kwargs.pop("headers", {})
+    headers["authorization"] = f"Bearer {TEST_TOKEN}"
     with requests.request(method, url, headers=headers, **kwargs) as response:
         response.raise_for_status()
         yield response
--- projects/analytics_server/tests/test_user.py
+++ projects/analytics_server/tests/test_user.py
@@ -6,7 +6,6 @@ from projects.analytics_server.tests.http_utils import Post, Get
 @pytest.mark.acceptance
 def test_user_sanity(cleanup_db):
     user_id = Post.create_user(admin_user)
-    print(user_id)
 
 
 @pytest.mark.acceptance
