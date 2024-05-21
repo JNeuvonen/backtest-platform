af3a141f1a3542b7885c8f9cab65caadfb18de4e
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-21 12:02:40 +0300

    [analytics_ww]: Create AuthProvider

---
+++ projects/analytics_www/src/context/auth.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from "react";
+import { Auth0Provider, AppState } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+interface Auth0ProviderWithHistoryProps {
+  children: ReactNode;
+}
+
+const Auth0ProviderWithHistory: React.FC<Auth0ProviderWithHistoryProps> = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+
+  console.log(process.env.REACT_APP_AUTH0_DOMAIN!);
+
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN!}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID!}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`,
+        scope:
+          "openid profile email read:current_user update:current_user_metadata",
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
