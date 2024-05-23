3fdc1ab92ee5cd037e5ded7ce232597fe54e0f1b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-23 09:59:35 +0300

    Update dev_prediction_server.sh

--- scripts/dev_prediction_server.sh
+++ scripts/dev_prediction_server.sh
@@ -1,11 +1,11 @@
-export DATABASE_URI="postgresql+psycopg2://jarnoneuvonen:password@localhost/pred_server_integration_tests"
+export DATABASE_URI="postgresql+psycopg2://postgres:salasana123@localhost/pred_server_integration_tests"
 # export DATABASE_URI=postgresql://postgres:HJyGHt67hRn8Ru@35.187.213.213:5432/prediction_server
 export SERVICE_PORT=8001
 export ENV=DEV
 export AUTO_WHITELISTED_IP="185.11.209.117"
 
 # npx kill-port $SERVICE_PORT && python realtime/prediction_server/src/main.py
-cd project 
+cd projects 
 cd prediction_server
 cd src
 
