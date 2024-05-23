661fec950e7ca4960f6f8659d055aa14d731ba86
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-23 13:40:25 +0300

    Update dev_analyticsweb.sh

--- scripts/dev_analyticsweb.sh
+++ scripts/dev_analyticsweb.sh
@@ -1,3 +1,9 @@
+chmod +x ./scripts/node_link_common_js.sh
+./scripts/node_link_common_js.sh
+
+
+export REACT_APP_ANALYTICS_SERV_URI="http://localhost:8000/"
+
 cd projects 
 cd analytics_www
 npm run start
