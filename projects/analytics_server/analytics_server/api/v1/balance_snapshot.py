393e2deced0f31129f7dca8daf1709804ab46a29
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-24 11:54:02 +0300

    [analytics_serv]: Add endpoint for fetching balance snapshots

--- projects/analytics_server/analytics_server/api/v1/balance_snapshot.py
+++ projects/analytics_server/analytics_server/api/v1/balance_snapshot.py
@@ -1,4 +1,6 @@
 from common_python.auth.oauth2 import verify_access_token
+from common_python.http_utils import HttpResponse
+from common_python.pred_serv_models.balance_snapshot import BalanceSnapshotQuery
 from fastapi import APIRouter, Depends
 
 
@@ -11,4 +13,6 @@ class RoutePaths:
 
 @router.get(RoutePaths.ROOT)
 async def get_balance_snapshots(token: str = Depends(verify_access_token)):
-    pass
+    with HttpResponse():
+        data = BalanceSnapshotQuery.read_all_entries()
+        return {"data": data}
