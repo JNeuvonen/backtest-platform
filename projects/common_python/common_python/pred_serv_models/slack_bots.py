0eb26136888490b2aa95606329fe27384e4d4a91
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-24 11:53:51 +0300

    [analytics_serv]: Fix circular imports error

--- projects/common_python/common_python/pred_serv_models/slack_bots.py
+++ projects/common_python/common_python/pred_serv_models/slack_bots.py
@@ -1,6 +1,5 @@
 from typing import Dict
 from sqlalchemy import Column, DateTime, Integer, String, func
-from common_python.log import LogExceptionContext
 from common_python.pred_serv_orm import Base, Session
 
 
@@ -19,48 +18,43 @@ class SlackWebhook(Base):
 class SlackWebhookQuery:
     @staticmethod
     def create_entry(fields: Dict):
-        with LogExceptionContext():
-            with Session() as session:
-                entry = SlackWebhook(**fields)
-                session.add(entry)
-                session.commit()
-                return entry.id
+        with Session() as session:
+            entry = SlackWebhook(**fields)
+            session.add(entry)
+            session.commit()
+            return entry.id
 
     @staticmethod
     def get_webhooks():
-        with LogExceptionContext():
-            with Session() as session:
-                return session.query(SlackWebhook).all()
+        with Session() as session:
+            return session.query(SlackWebhook).all()
 
     @staticmethod
     def update_webhook(webhook_id, update_fields: Dict):
-        with LogExceptionContext():
-            with Session() as session:
-                update_fields.pop("id", None)
-                non_null_update_fields = {
-                    k: v for k, v in update_fields.items() if v is not None
-                }
-                session.query(SlackWebhook).filter(
-                    SlackWebhook.id == webhook_id
-                ).update(non_null_update_fields, synchronize_session="fetch")
-                session.commit()
+        with Session() as session:
+            update_fields.pop("id", None)
+            non_null_update_fields = {
+                k: v for k, v in update_fields.items() if v is not None
+            }
+            session.query(SlackWebhook).filter(SlackWebhook.id == webhook_id).update(
+                non_null_update_fields, synchronize_session="fetch"
+            )
+            session.commit()
 
     @staticmethod
     def get_webhook_by_id(webhook_id: int):
-        with LogExceptionContext():
-            with Session() as session:
-                return (
-                    session.query(SlackWebhook)
-                    .filter(SlackWebhook.id == webhook_id)
-                    .first()
-                )
+        with Session() as session:
+            return (
+                session.query(SlackWebhook)
+                .filter(SlackWebhook.id == webhook_id)
+                .first()
+            )
 
     @staticmethod
     def get_webhook_by_name(webhook_name: str):
-        with LogExceptionContext():
-            with Session() as session:
-                return (
-                    session.query(SlackWebhook)
-                    .filter(SlackWebhook.name == webhook_name)
-                    .first()
-                )
+        with Session() as session:
+            return (
+                session.query(SlackWebhook)
+                .filter(SlackWebhook.name == webhook_name)
+                .first()
+            )
