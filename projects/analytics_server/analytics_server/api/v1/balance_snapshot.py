eaa5a3e019422af259c8f9e87d995a8cab3186a5
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-30 23:08:36 +0300

    [analytics_serv]: Add utility functions for balance_snapshots

--- projects/analytics_server/analytics_server/api/v1/balance_snapshot.py
+++ projects/analytics_server/analytics_server/api/v1/balance_snapshot.py
@@ -9,10 +9,28 @@ router = APIRouter()
 
 class RoutePaths:
     ROOT = "/"
+    LATEST = "/latest"
+    ONE_DAY_INTERVAL = "/1d-inteval"
 
 
 @router.get(RoutePaths.ROOT)
 async def get_balance_snapshots(token: str = Depends(verify_access_token)):
     with HttpResponse():
         data = BalanceSnapshotQuery.read_all_entries()
+        return {"data": data}
+
+
+@router.get(RoutePaths.LATEST)
+async def get_latest_balance_snapshot(token: str = Depends(verify_access_token)):
+    with HttpResponse():
+        data = BalanceSnapshotQuery.get_last_snapshot()
+        return {"data": data}
+
+
+@router.get(RoutePaths.ONE_DAY_INTERVAL)
+async def get_balance_snapshots_one_day_interval(
+    token: str = Depends(verify_access_token),
+):
+    with HttpResponse():
+        data = BalanceSnapshotQuery.get_snapshots_with_one_day_interval()
         return {"data": data}
