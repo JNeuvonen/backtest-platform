22188fe6d82849218964456459affb86e45b3a98
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-05-30 12:02:04 +0300

    [analytics_serv]: Add ls_tickers and ls_pairs to GET strats endpoint

--- projects/analytics_server/analytics_server/api/v1/strategies.py
+++ projects/analytics_server/analytics_server/api/v1/strategies.py
@@ -4,7 +4,9 @@ from common_python.pred_serv_models.strategy import StrategyQuery
 from common_python.pred_serv_models.longshortgroup import LongShortGroupQuery
 from common_python.pred_serv_models.trade import TradeQuery
 from common_python.pred_serv_orm import StrategyGroup
+from common_python.pred_serv_models.longshortticker import LongShortTickerQuery
 from common_python.pred_serv_models.strategy_group import StrategyGroupQuery
+from common_python.pred_serv_models.longshortpair import LongShortPairQuery
 from fastapi import APIRouter, Depends
 
 
@@ -22,9 +24,13 @@ async def get_strategies(token: str = Depends(verify_access_token)):
         directional_strategies = StrategyQuery.get_strategies()
         trades = TradeQuery.get_trades()
         strategy_groups = StrategyGroupQuery.get_all()
+        ls_tickers = LongShortTickerQuery.get_all()
+        pairs = LongShortPairQuery.get_all()
 
         return {
             "ls_strategies": ls_strategies,
+            "ls_tickers": ls_tickers,
+            "ls_pairs": pairs,
             "directional_strategies": directional_strategies,
             "trades": trades,
             "strategy_groups": strategy_groups,
