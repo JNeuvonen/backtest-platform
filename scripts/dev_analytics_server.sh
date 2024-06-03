a70d41756a94b2d20e9dc05f3b908859cb3da510
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-06-03 16:26:01 +0300

    Chore: update analytics startup scripts

--- scripts/dev_analytics_server.sh
+++ scripts/dev_analytics_server.sh
@@ -1,6 +1,6 @@
 export DATABASE_URI="postgresql://postgres:salasana123@localhost:5432/live_env_db_dump"
 
-export SERVICE_PORT=8000
+export SERVICE_PORT=8003
 export ENV=DEV
 export AUTH0_DOMAIN="dev-3db8jwnabrjinvzd.us.auth0.com"
 export AUTH0_API_IDENTIFIER="https://dev-3db8jwnabrjinvzd.us.auth0.com/api/v2/"
--- scripts/dev_analyticsweb.sh
+++ scripts/dev_analyticsweb.sh
@@ -2,7 +2,7 @@ chmod +x ./scripts/node_link_common_js.sh
 ./scripts/node_link_common_js.sh
 
 
-export REACT_APP_ANALYTICS_SERV_URI="http://localhost:8000/"
+export REACT_APP_ANALYTICS_SERV_URI="http://localhost:8003/"
 
 npx kill-port 3000
 
