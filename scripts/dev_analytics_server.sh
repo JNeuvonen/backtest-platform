102b7ddf7f36cec148686ccad5f1be05e2da16da
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-06-26 16:43:52 +0300

    [analytics_serv]: Remove binance api keys from startup script

--- scripts/dev_analytics_server.sh
+++ scripts/dev_analytics_server.sh
@@ -5,8 +5,8 @@ export ENV=DEV
 export AUTH0_DOMAIN="dev-3db8jwnabrjinvzd.us.auth0.com"
 export AUTH0_API_IDENTIFIER="https://dev-3db8jwnabrjinvzd.us.auth0.com/api/v2/"
 
-export BINANCE_API_KEY="EnTX8mUnaxbxYb6REHcb63nXMbK0uBBrpSKtrUnZKWWf18bub89dK5BtjqgK25x4"
-export BINANCE_API_SECRET="bnSykCRO3Q6sCJMNUhKIgi7KyhpWJZi6BkUG9bbUmc5XzSmawK2xwVPpRCzjVQ8n"
+export BINANCE_API_KEY=""
+export BINANCE_API_SECRET=""
 export LOG_SOURCE_PROGRAM="3"
 
 
