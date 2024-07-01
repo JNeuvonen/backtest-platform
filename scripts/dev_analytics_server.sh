464211e84e4d74f67c0dd4cf7a1fd43cee72b5b9
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2024-07-01 17:20:22 +0300

    Chore: dev_analytics_server.sh - force user to pass in binance secrets during runtime

--- scripts/dev_analytics_server.sh
+++ scripts/dev_analytics_server.sh
@@ -1,14 +1,26 @@
-export DATABASE_URI="postgresql://postgres:salasana123@localhost:5432/live_env_db_dump"
+#!/bin/bash
 
+export DATABASE_URI="postgresql://postgres:password@localhost:5432/algorithmic_trading_local"
 export SERVICE_PORT=8003
 export ENV=DEV
 export AUTH0_DOMAIN="dev-3db8jwnabrjinvzd.us.auth0.com"
 export AUTH0_API_IDENTIFIER="https://dev-3db8jwnabrjinvzd.us.auth0.com/api/v2/"
 
-export BINANCE_API_KEY=""
-export BINANCE_API_SECRET=""
-export LOG_SOURCE_PROGRAM="3"
+# Check if BINANCE_API_KEY is set
+if [ -z "${BINANCE_API_KEY}" ]; then
+  echo "Error: BINANCE_API_KEY is not set."
+  exit 1
+fi
 
+# Check if BINANCE_API_SECRET is set
+if [ -z "${BINANCE_API_SECRET}" ]; then
+  echo "Error: BINANCE_API_SECRET is not set."
+  exit 1
+fi
+
+export BINANCE_API_KEY=${BINANCE_API_KEY}
+export BINANCE_API_SECRET=${BINANCE_API_SECRET}
+export LOG_SOURCE_PROGRAM="3"
 
 cd projects 
 cd analytics_server 
